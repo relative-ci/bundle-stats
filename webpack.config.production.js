c376b72864892348db550a93f3c0e96c01ede5fb
Vio <vio@beanon.com>
2018-02-11 22:58:57 +0100

    Add Netlify redirects

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,10 +1,22 @@
+const path = require('path');
 const webpackMerge = require('webpack-merge');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const commonConfig = require('./webpack.config.common');
+
+const projectDir = __dirname;
+const distDir = path.resolve(projectDir, 'dist');
 
 module.exports = webpackMerge(commonConfig, {
   output: {
     filename: '[name].[chunkhash:8].js',
   },
-  plugins: [],
+  plugins: [
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, 'public'),
+        to: distDir,
+      },
+    ]),
+  ],
 });
