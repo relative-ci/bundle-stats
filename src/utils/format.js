7c51e84eae0315f6bedbc2da8d3e0e27806dd9a1
Vio <vio@beanon.com>
2019-01-05 09:30:11 +0100

    feat: Add format utilities

---
+++ src/utils/format.js
@@ -0,0 +1,18 @@
+import convert from 'convert-units';
+import { round } from 'lodash';
+
+export const formatFileSize = (val) => {
+  const res = convert(val).from('B').toBest();
+  return `${round(res.val, 2)}${res.unit}`;
+};
+
+export const formatDuration = (val) => {
+  const res = convert(val).from('ms').toBest();
+  return `${round(res.val, 4)}${res.unit}`;
+};
+
+export const formatNumber = val => val;
+
+export const formatCommit = commitSha => (commitSha && commitSha.substr(0, 7)) || '';
+
+export const formatPercentage = val => `${val}%`;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export * from './delta';
+export * from './format';
 export * from './get-asset-name';
 export * from './get-assets-metrics';
