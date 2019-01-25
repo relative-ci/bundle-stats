4b3d6bd52f67fe305ad604d98bce743dd5e1aa47
Vio <vio@beanon.com>
2019-01-25 23:21:49 +0100

    feat: getAssetName - add support for "-" separated hash

--- src/utils/get-asset-name.js
+++ src/utils/get-asset-name.js
@@ -1,5 +1,25 @@
 /**
  * Extract (guess) filename from a hashed filename
  */
+import { last } from 'lodash';
 
-export const getAssetName = assetFilename => assetFilename.replace(/\.[a-f0-9]{5,32}\./, '.');
+const FILENAME_HASH_PATTERN = /[.|-][a-f0-9]{5,32}$/;
+
+export const getAssetName = (assetFilepath) => {
+  const pathParts = assetFilepath.split('/');
+  const dirname = pathParts.slice(0, -1).join('/');
+  const filename = last(pathParts);
+
+  const filenameParts = filename.split('.');
+
+  const { basename, extension } = filenameParts.slice(-2, -1).join('') === 'min'
+    ? {
+      basename: filenameParts.slice(0, -2).join('.'),
+      extension: filenameParts.slice(-2).join('.'),
+    } : {
+      basename: filenameParts.slice(0, -1).join('.'),
+      extension: filenameParts.slice(-1).join('.'),
+    };
+
+  return `${dirname ? `${dirname}/` : ''}${basename.replace(FILENAME_HASH_PATTERN, '')}.${extension}`;
+};
