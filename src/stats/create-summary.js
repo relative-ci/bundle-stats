67b6f09bdbee182e7e8d51014e692c2f394cbefe
Vio <vio@beanon.com>
2019-03-28 18:24:54 +0100

    feat: createStatsSummary

---
+++ src/stats/create-summary.js
@@ -0,0 +1,25 @@
+const { get, merge } = require('lodash');
+
+export const createStatsSummary = (baselineStats, currentStats) => {
+  const metrics = [
+    'webpack.assets.totalSizeByTypeALL',
+    'webpack.assets.totalInitialSizeJS',
+    'webpack.assets.totalInitialSizeCSS',
+    'webpack.cacheInvalidation',
+    'webpack.modulesCount',
+    'webpack.chunksCount',
+    'webpack.assetsCount',
+  ];
+
+  return metrics.map((metric) => {
+    const baselineValue = get(baselineStats, `${metric}.value`, 0);
+    const currentValue = get(currentStats, `${metric}.value`, 0);
+
+    return {
+      [metric]: {
+        baseline: baselineValue,
+        current: currentValue,
+      },
+    };
+  }).reduce((agg, current) => merge({}, agg, current), {});
+};
