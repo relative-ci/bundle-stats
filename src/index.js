171d171841445ba97972f52b767953a3fd7205be
Vio <vio@beanon.com>
2019-04-07 23:24:34 +0200

    feat: Export logo & logotype

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import LogoComponent from './images/relative-ci-logo.inline.svg';
+import LogotypeComponent from './images/relative-ci-logotype.inline.svg';
+
+export const Logo = LogoComponent;
+export const Logotype = LogotypeComponent;
 export * from './components';
 export * from './ui';
 export * from './layout';
