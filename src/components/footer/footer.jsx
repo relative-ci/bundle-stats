ba636ee3b11281489057c15cb0f891d702ccea47
Vio <vio@beanon.com>
2019-04-07 11:56:35 +0200

    feat: Footer - add logo

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
+import Logo from '../../images/relative-ci-logo.inline.svg';
 import { Container } from '../../ui';
 import css from './footer.module.css';
 
@@ -23,82 +24,97 @@ StandardLink.propTypes = {
 
 export const Footer = ({ className, Link }) => (
   <footer className={cx(css.root, className)}>
-    <Container className={css.container}>
-      <div className={css.navigation}>
-        <div className={css.navGroup}>
-          <h3 className={css.navGroupTitle}>
-            Documentation
-          </h3>
-          <nav className={css.nav}>
-            <Link
-              to="/documentation/setup"
-              className={css.navLink}
-            >
-              Setup
-            </Link>
-            <Link
-              to="/documentation/resources"
-              className={css.navLink}
-            >
-              Resources
-            </Link>
-          </nav>
+    <Container>
+      <div className={css.info}>
+        <div className={css.navigation}>
+          <div className={css.navGroup}>
+            <h3 className={css.navGroupTitle}>
+              Documentation
+            </h3>
+            <nav className={css.nav}>
+              <Link
+                to="/documentation/setup"
+                className={css.navLink}
+              >
+                Setup
+              </Link>
+              <Link
+                to="/documentation/resources"
+                className={css.navLink}
+              >
+                Resources
+              </Link>
+            </nav>
+          </div>
+
+          <div className={css.navGroup}>
+            <h3 className={css.navGroupTitle}>
+              Tools
+            </h3>
+            <nav className={css.nav}>
+              <a
+                href="https://compare.relative-ci.com/"
+                className={css.navLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Side by side comparison for webpack/lighthouse/browsertime stats"
+              >
+                Compare
+              </a>
+              <a
+                href="https://video-compare.relative-ci.com/"
+                className={css.navLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Side by side video playing for webpagetest / browsertime recordings."
+              >
+                Video Compare
+              </a>
+            </nav>
+          </div>
+
+          <div className={css.navGroup}>
+            <h3 className={css.navGroupTitle}>
+              Social
+            </h3>
+            <nav className={css.nav}>
+              <a
+                href="https://github.com/relative-ci"
+                className={css.navLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+              <a
+                href="https://twitter.com/Relative_CI"
+                className={css.navLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Twitter
+              </a>
+            </nav>
+          </div>
         </div>
 
-        <div className={css.navGroup}>
-          <h3 className={css.navGroupTitle}>
-            Tools
-          </h3>
-          <nav className={css.nav}>
-            <a
-              href="https://compare.relative-ci.com/"
-              className={css.navLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              title="Side by side comparison for webpack/lighthouse/browsertime stats"
-            >
-              Compare
-            </a>
-            <a
-              href="https://video-compare.relative-ci.com/"
-              className={css.navLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              title="Side by side video playing for webpagetest / browsertime recordings."
-            >
-              Video Compare
-            </a>
-          </nav>
-        </div>
-
-        <div className={css.navGroup}>
-          <h3 className={css.navGroupTitle}>
-            Social
-          </h3>
-          <nav className={css.nav}>
-            <a
-              href="https://github.com/relative-ci"
-              className={css.navLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://twitter.com/Relative_CI"
-              className={css.navLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Twitter
-            </a>
-          </nav>
-        </div>
+        <p className={css.copyright}>
+          &copy; 2019 Relative CI
+        </p>
       </div>
 
-      <p className={css.copyright}>
-        &copy; 2019 Relative CI
-      </p>
+      <div className={css.branding}>
+        <Link
+          className={css.brandingHome}
+          to="/"
+          title="Go to homepage"
+        >
+          <Logo className={css.brandingLogo} />
+          <span className={css.brandingLogotype}>
+            Relative CI
+          </span>
+        </Link>
+      </div>
     </Container>
   </footer>
 );
