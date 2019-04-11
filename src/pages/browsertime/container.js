70b5db401382e349c1d76515ac07785c8916ea50
Vio <vio@beanon.com>
2019-04-11 22:34:19 +0200

    feat: Use BrowsertimeTable

--- src/pages/browsertime/container.js
+++ src/pages/browsertime/container.js
@@ -1,32 +1,7 @@
-import { compose } from 'recompose';
+import { compose, withProps } from 'recompose';
 
 import withSources from '../../hocs/with-sources';
 import withRuns from '../../hocs/with-runs';
-import withMetrics from '../../hocs/with-metrics';
-
-const metricsMap = {
-  'browsertime.firstPaint': 'statistics.timings.firstPaint.median',
-  'browsertime.fullyLoaded': 'statistics.timings.fullyLoaded.median',
-  'browsertime.backEndTime': 'statistics.timings.pageTimings.backEndTime.median',
-  'browsertime.domContentLoadedTime': 'statistics.timings.pageTimings.domContentLoadedTime.median',
-  'browsertime.domInteractiveTime': 'statistics.timings.pageTimings.domInteractiveTime.median',
-  'browsertime.domainLookupTime': 'statistics.timings.pageTimings.domainLookupTime.median',
-  'browsertime.frontEndTime': 'statistics.timings.pageTimings.frontEndTime.median',
-  'browsertime.pageDownloadTime': 'statistics.timings.pageTimings.pageDownloadTime.median',
-  'browsertime.pageLoadTime': 'statistics.timings.pageTimings.pageLoadTime.median',
-  'browsertime.redirectionTime': 'statistics.timings.pageTimings.redirectionTime.median',
-  'browsertime.serverConnectionTime': 'statistics.timings.pageTimings.serverConnectionTime.median',
-  'browsertime.serverResponseTime': 'statistics.timings.pageTimings.serverResponseTime.median',
-  'browsertime.firstContentfulPaint': 'statistics.timings.paintTiming.first-contentful-paint.median',
-  'browsertime.rumSpeedIndex': 'statistics.timings.rumSpeedIndex.median',
-  'browsertime.firstVisualChange': 'statistics.visualMetrics.FirstVisualChange.median',
-  'browsertime.lastVisualChange': 'statistics.visualMetrics.LastVisualChange.median',
-  'browsertime.perceptualSpeedIndex': 'statistics.visualMetrics.PerceptualSpeedIndex.median',
-  'browsertime.speedIndex': 'statistics.visualMetrics.SpeedIndex.median',
-  'browsertime.visualComplete85': 'statistics.visualMetrics.VisualComplete85.median',
-  'browsertime.visualComplete95': 'statistics.visualMetrics.VisualComplete95.median',
-  'browsertime.visualComplete99': 'statistics.visualMetrics.VisualComplete99.median',
-};
 
 const metaMap = {
   timestamp: 'info.timestamp',
@@ -34,10 +9,19 @@ const metaMap = {
   connectivity: 'info.connectivity.profile',
 };
 
+const createJobs = sources => sources.map(({ res }, index) => ({
+  internalBuildNumber: index + 1,
+  rawData: {
+    browsertime: res,
+  },
+}));
+
 const enhance = compose(
   withSources(),
   withRuns(metaMap),
-  withMetrics(),
+  withProps(({ sources }) => ({
+    jobs: createJobs(sources),
+  })),
 );
 
 export default enhance;
