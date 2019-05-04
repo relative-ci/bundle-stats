3b5b3dd8485da0079a545c9863c7ba45459ed625
Vio <vio@beanon.com>
2019-05-04 12:53:09 +0200

    build: Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   setupFiles: ['<rootDir>/build/jest/register-context.js'],
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
+    '^.+\\.md?$': 'markdown-loader-jest',
   },
   testEnvironment: 'node',
 };
