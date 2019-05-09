69ddb9c74a09f7d5207a47035fd163379e485e3b
Vio <vio@beanon.com>
2019-05-09 23:34:31 +0200

    build: Add Eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
   extends: 'airbnb-base',
-  plugins: ['jest'],
-  global: {
-    'test': true,
-    'expect': true,
-  },
   rules: {
     'import/prefer-default-export': 0,
   }
