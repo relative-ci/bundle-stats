78ba935805ca4a5ae12689d27e0fc36e84fcdbe9
Vio <vio@beanon.com>
2019-05-09 23:49:34 +0200

    build: Add Jest setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
   extends: 'airbnb-base',
+  plugins: ['jest'],
+  globals: {
+    'test': true,
+    'expect': true,
+  },
   rules: {
     'import/prefer-default-export': 0,
   }
