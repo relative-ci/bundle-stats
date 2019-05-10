0f57c6567565b27a8e79b38dc0f0e22470c4dc95
Vio <vio@beanon.com>
2019-05-10 08:56:42 +0200

    feat: Generate report page

--- src/create-report.js
+++ src/create-report.js
@@ -1,14 +1,13 @@
-import path from 'path';
 import { readFileSync } from 'fs-extra';
 import { createJobs } from './create-jobs';
 
-const TEMPLATE_FILEPATH = path.join(__dirname, './report.html');
-const INITIAL_DATA_PATTERN = /"__INITIAL_DATA_PLACEHOLDER__"/;
+const TEMPLATE_FILEPATH = require.resolve('@relative-ci/webpack-bundle-stats-html-template');
+const INITIAL_DATA_PATTERN = /window\.__INITIAL_DATA__ = \[\]/;
 
 export const createReport = (artifacts) => {
   const initialData = createJobs(artifacts);
 
   // @FIXME
   const template = readFileSync(TEMPLATE_FILEPATH, 'utf-8');
-  return template.replace(INITIAL_DATA_PATTERN, JSON.stringify(initialData));
+  return template.replace(INITIAL_DATA_PATTERN, `window.__INITIAL_DATA__ = ${JSON.stringify(initialData)}`);
 };
