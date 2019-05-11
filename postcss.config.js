aa8c28b745bf3f9bc86da59159388dd237fd4d13
Vio <vio@beanon.com>
2019-05-11 17:43:00 +0200

    build: Enable css minification

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 const stylelint = require('stylelint');
 const autoprefix = require('autoprefixer');
+const cssnano = require('cssnano');
 
-module.exports = {
+module.exports = ctx => ({
   plugins: [
     stylelint,
     autoprefix,
+    ...(ctx.env === 'production' ? [cssnano] : []),
   ],
-};
+});
