a1efc13044efc8e1a28dd9ce4bbfbc9b22e75a97
Vio <vio@beanon.com>
2019-05-12 11:55:49 +0200

    test: Standalone stories data

--- src/apps/standalone/stories.jsx
+++ src/apps/standalone/stories.jsx
@@ -1,10 +1,22 @@
 /* global module */
 import { storiesOf } from '@storybook/react';
+import { createJob } from '@relative-ci/utils';
 
-import StandaloneApp from './';
+import currentFixtures from '../../../fixtures/webpack-stats-1.extracted';
+import baselineFixtures from '../../../fixtures/webpack-stats-2.extracted';
+import StandaloneApp from '.';
+
+const BASELINE_JOB = {
+  ...createJob({ webpack: { stats: baselineFixtures } }),
+  internalBuildNumber: 2,
+};
+const CURRENT_JOB = {
+  ...createJob({ webpack: { stats: currentFixtures } }, BASELINE_JOB),
+  internalBuildNumber: 1,
+};
 
 const stories = storiesOf('StandaloneApp', module);
 
 stories.add('default', () => (
-  <StandaloneApp />
+  <StandaloneApp jobs={[CURRENT_JOB, BASELINE_JOB]} />
 ));
