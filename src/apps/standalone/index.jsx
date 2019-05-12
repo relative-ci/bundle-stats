5615853ad51366ab87953b37b371d38da59795e0
Vio <vio@beanon.com>
2019-05-12 14:24:24 +0200

    feat: Standalone - update layout

--- src/apps/standalone/index.jsx
+++ src/apps/standalone/index.jsx
@@ -3,32 +3,52 @@ import {
   Container, Summary, BundleAssets, BundleAssetsTotalsTable, BundleModules,
 } from '@relative-ci/ui';
 
+import css from './styles.css';
+
+const Header = () => (
+  <Container className={css.header}>
+    <h1 className={css.title}>
+      Webpack bundle stats
+    </h1>
+  </Container>
+);
+
 const StandaloneApp = ({ jobs }) => {
   if (jobs.length === 0) {
     return (
-      <Container>
-        <p>No data available.</p>
-      </Container>
+      <div>
+        <Header />
+        <div className={css.main}>
+          <Container>
+            <div className={css.empty}>
+              No data available.
+            </div>
+          </Container>
+        </div>
+      </div>
     );
   }
 
   return (
     <div>
-      <Container>
-        <Summary data={jobs[0].summary} />
-      </Container>
-      <Container>
-        <h2>Totals</h2>
-        <BundleAssetsTotalsTable jobs={jobs} />
-      </Container>
-      <Container>
-        <h2>Assets</h2>
-        <BundleAssets jobs={jobs} />
-      </Container>
-      <Container>
-        <h2>Modules</h2>
-        <BundleModules jobs={jobs} />
-      </Container>
+      <Header />
+      <div className={css.main}>
+        <Container>
+          <Summary data={jobs[0].summary} />
+        </Container>
+        <Container>
+          <h2>Totals</h2>
+          <BundleAssetsTotalsTable jobs={jobs} />
+        </Container>
+        <Container>
+          <h2>Assets</h2>
+          <BundleAssets jobs={jobs} />
+        </Container>
+        <Container>
+          <h2>Modules</h2>
+          <BundleModules jobs={jobs} />
+        </Container>
+      </div>
     </div>
   );
 };
