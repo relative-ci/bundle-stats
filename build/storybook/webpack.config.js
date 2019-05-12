577556853b6d9a7b261f36bf0894f67586f8ea04
Vio <vio@beanon.com>
2019-05-12 11:48:46 +0200

    build: Fix storybook config

--- build/storybook/webpack.config.js
+++ build/storybook/webpack.config.js
@@ -4,9 +4,14 @@ const appCssConfig = require('../webpack/configs/css');
 const appResolveConfig = require('../webpack/configs/resolve');
 const appFilesConfig = require('../webpack/configs/files');
 
-module.exports = storybookConfig => webpackMerge.smart(
-  storybookConfig,
-  appCssConfig,
-  appResolveConfig,
-  appFilesConfig,
-);
+module.exports = ({ config }) => {
+  // Remove other rules than js & md
+  config.module.rules = config.module.rules.slice(0, 2); // eslint-disable-line no-param-reassign
+
+  return webpackMerge.smart(
+    config,
+    appCssConfig,
+    appResolveConfig,
+    appFilesConfig,
+  );
+};
