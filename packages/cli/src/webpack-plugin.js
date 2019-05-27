15cb4e5b084f6e856622ca8eb5daccf39eef5994
Vio <vio@beanon.com>
2019-05-28 01:03:09 +0200

    feat: Add webpack-plugin

---
+++ packages/cli/src/webpack-plugin.js
@@ -0,0 +1,67 @@
+const process = require('process');
+const path = require('path');
+const { get, merge } = require('lodash');
+const { createJobs } = require('./create-jobs');
+const { createReports } = require('./create-report');
+
+const CWD = process.cwd();
+
+const DEFAULT_OPTIONS = {
+  html: true,
+  json: false,
+  outDir: '',
+  stats: {
+    context: CWD,
+    assets: true,
+    entrypoints: true,
+    chunks: true,
+    modules: true,
+  },
+};
+
+const getOnEmit = options => (compilation, callback) => {
+  const { outDir, stats: statsOptions } = options;
+  const data = compilation.getStats().toJson(statsOptions);
+  const artifacts = createJobs([data]);
+
+  const reportTypes = [
+    ...options.html ? ['html'] : [],
+    ...options.json ? ['json'] : [],
+  ];
+
+  createReports(artifacts, reportTypes)
+    .then((reports) => {
+      reports.forEach(({ type, output }) => {
+        const filepath = path.join(outDir, `report.${type}`);
+
+        // eslint-disable-next-line no-param-reassign
+        compilation.assets[filepath] = {
+          size: () => output.length,
+          source: () => output,
+        };
+      });
+    })
+    .then(callback)
+    .catch(err => callback(err));
+};
+
+export class BundleStatsWebpackPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    const options = merge(
+      {},
+      DEFAULT_OPTIONS,
+      {
+        stats: {
+          context: get(compiler, 'options.context'),
+        },
+      },
+      this.options,
+    );
+
+    compiler.hooks.emit.tapAsync('BundleStats', getOnEmit(options));
+  }
+}
