cd95d9fa8fdbc7f39d2b9855644ea8efb198d83b
Vio <vio@beanon.com>
2019-07-07 20:57:28 +0200

    feat(html-template): Remove logo

--- packages/html-templates/src/app/header/header.jsx
+++ packages/html-templates/src/app/header/header.jsx
@@ -11,14 +11,6 @@ export const Header = ({ className }) => (
     className={cx(css.root, className)}
     renderLeft={partProps => (
       <div {...partProps}>
-        <Logo
-          className={css.webpackLogo}
-          kind="webpack"
-          as="a"
-          href="https://webpack.js.org"
-          target="_blank"
-          rel="noopener noreferrer nofollow"
-        />
         <h1 className={css.title}>
           Bundle Stats
         </h1>
