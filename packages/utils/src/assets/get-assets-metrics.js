01c39a1fba376b5acc09878a355504b1ee75598b
Vio <vio@beanon.com>
2019-07-11 18:43:37 +0200

    feat(utils): getAssetsMetrics - add asset data

--- packages/utils/src/assets/get-assets-metrics.js
+++ packages/utils/src/assets/get-assets-metrics.js
@@ -1,3 +1,5 @@
+import { get } from 'lodash';
+
 import { getAssetName } from './get-asset-name';
 
 const IGNORE_PATTERN = /\.map$/;
@@ -5,22 +7,44 @@ const IGNORE_PATTERN = /\.map$/;
 /*
  * Transform assets array to an object with metrics
  */
-export const getAssetsMetrics = (assets = []) => assets.reduce((aggregator, asset) => {
-  if (IGNORE_PATTERN.test(asset.name)) {
-    return aggregator;
-  }
+export const getAssetsMetrics = (assets = [], data) => {
+  const chunks = get(data, 'chunks', []);
 
-  const source = getAssetName(asset.name);
-  // @TODO Get an uniq id (based on url, source)
-  const id = source;
+  const entryItems = Object.values(chunks)
+    .filter(({ entry }) => entry)
+    .map(({ files }) => files)
+    .flat();
 
-  const { size, ...restAssetProps } = asset;
+  const initialItems = Object.values(chunks)
+    .filter(({ initial }) => initial)
+    .map(({ files }) => files)
+    .flat();
 
-  return {
-    ...aggregator,
-    [id]: {
-      ...restAssetProps,
-      value: size,
-    },
-  };
-}, {});
+  const chunkItems = Object.values(chunks)
+    .filter(({ entry, initial }) => !entry && !initial)
+    .map(({ files }) => files)
+    .flat();
+
+  return assets.reduce((aggregator, asset) => {
+    if (IGNORE_PATTERN.test(asset.name)) {
+      return aggregator;
+    }
+
+    const source = getAssetName(asset.name);
+    // @TODO Get an uniq id (based on url, source)
+    const id = source;
+
+    const { size, name } = asset;
+
+    return {
+      ...aggregator,
+      [id]: {
+        name,
+        value: size,
+        isEntry: entryItems.includes(name),
+        isInitial: initialItems.includes(name),
+        isChunk: chunkItems.includes(name),
+      },
+    };
+  }, {});
+};
