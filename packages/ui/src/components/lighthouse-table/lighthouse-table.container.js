b7cc20e53162308e2a8afd6cb1e19aa91a7fb505
Vio <vio@beanon.com>
2019-07-16 23:26:27 +0200

    refactor(ui): LighthouseTable - use getLighthouseMetrics from utils

--- packages/ui/src/components/lighthouse-table/lighthouse-table.container.js
+++ packages/ui/src/components/lighthouse-table/lighthouse-table.container.js
@@ -1,37 +1,6 @@
 import { compose, withProps } from 'recompose';
-import {
-  get, map, mean, round,
-} from 'lodash';
-import { addMetricsData, mergeRunsById } from '@bundle-stats/utils';
-
-const getScore = (res) => {
-  const scores = map(res.reportCategories, 'score');
-  return round(mean(scores), 2);
-};
-
-const METRICS = {
-  'lighthouse.score': getScore,
-  'lighthouse.speedIndex': 'audits.speed-index-metric.rawValue',
-  'lighthouse.firstMeaningfulPain': 'audits.first-meaningful-paint.rawValue',
-  'lighthouse.timeToFirstByte': 'audits.time-to-first-byte.rawValue',
-  'lighthouse.firstInteractive': 'audits.first-interactive.rawValue',
-  'lighthouse.totalByteWeight': 'audits.total-byte-weight.rawValue',
-  'lighthouse.domSize': 'audits.dom-size.rawValue',
-};
-
-const getLighthouseMetrics = (data) => {
-  if (!data) {
-    return {};
-  }
-
-  return Object.entries(METRICS)
-    .reduce((agg, [metricKey, metricPath]) => ({
-      ...agg,
-      [metricKey]: {
-        value: typeof metricPath === 'function' ? metricPath(data) : get(data, metricPath, 0),
-      },
-    }), {});
-};
+import { get, map } from 'lodash';
+import { getLighthouseMetrics, addMetricsData, mergeRunsById } from '@bundle-stats/utils';
 
 export const enhance = compose(
   withProps(({ jobs }) => {
