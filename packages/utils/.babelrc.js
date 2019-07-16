8e8c955e3d820236990b565d5dfa556fb3687368
Vio <vio@beanon.com>
2019-07-16 22:57:44 +0200

    build(utils): Setup core-js for node v8

--- packages/utils/.babelrc.js
+++ packages/utils/.babelrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', {
+      useBuiltIns: 'usage',
+      corejs: 3,
       targets: {
-        node: '8.0.0',
+        node: 'current',
       },
     }],
   ],
