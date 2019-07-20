2376493ae29e7492466c4d1ea0fc06d9b3911565
Vio <vio@beanon.com>
2019-07-20 23:50:45 +0200

    feat(ui): Box - remove inner spacing

--- packages/ui/src/ui/box/box.stories.jsx
+++ packages/ui/src/ui/box/box.stories.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { getWrapperDecorator } from '../../stories';
+import { Table } from '../table';
 import { Box } from '.';
 
 const stories = storiesOf('UI/Box', module);
@@ -10,5 +11,24 @@ stories.addDecorator(getWrapperDecorator());
 stories.add('default', () => (
   <Box>
     <p>Lorem ipsum</p>
+  </Box>
+));
+
+stories.add('with table', () => (
+  <Box>
+    <Table
+      headers={['Col A', 'Col B', 'Col C']}
+      rows={[
+        {
+          cells: ['a1', 'b1', 'c1'],
+        },
+        {
+          cells: ['a2', 'b2', 'c2'],
+        },
+        {
+          cells: ['a3', 'b3', 'c3'],
+        },
+      ]}
+    />
   </Box>
 ));
