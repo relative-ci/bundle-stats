d8f88364ea2cc9e22445978249dfb3ada94ceb9f
Vio <vio@beanon.com>
2019-08-29 11:52:03 +0200

    feat(ui): Alert component

---
+++ packages/ui/src/ui/alert/alert.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+
+import css from './alert.module.css';
+
+export const Alert = (props) => {
+  const { className, kind, children } = props;
+
+  return (
+    <div className={cx(css.root, className, css[kind])}>
+      {children}
+    </div>
+  );
+};
+
+Alert.propTypes = {
+  className: PropTypes.string,
+  kind: PropTypes.string,
+  children: PropTypes.element.isRequired,
+};
+
+Alert.defaultProps = {
+  className: '',
+  kind: 'default',
+};
