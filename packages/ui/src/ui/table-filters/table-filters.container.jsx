14a67889940c48401bf194d80f721615b7d7dea3
Vio <vio@beanon.com>
2019-09-04 19:16:55 +0200

    style(ui): TableFilters - fix formatting

--- packages/ui/src/ui/table-filters/table-filters.container.jsx
+++ packages/ui/src/ui/table-filters/table-filters.container.jsx
@@ -23,70 +23,74 @@ const getInitialValues = (key, filters) => {
   return {};
 };
 
-export const tableFiltersContainer = (BaseComponent) => class TableFiltersContainer extends React.Component {
-  static defaultProps = {
-    onChange: null,
+export const tableFiltersContainer = (BaseComponent) => {
+  class TableFiltersContainer extends React.Component {
+    constructor(props) {
+      super(props);
+
+      const { filters } = this.props;
+
+      this.state = {
+        values: getInitialValues('', filters),
+        open: false,
+      };
+    }
+
+    toggleFilter = (key, value) => this.setState(({ values, ...state }) => {
+      const newState = {
+        ...state,
+        values: {
+          ...values,
+          [key]: value,
+        },
+      };
+
+      const { onChange } = this.props;
+
+      if (onChange) {
+        onChange(newState.values);
+      }
+
+      return newState;
+    })
+
+    dropdownToggle = () => this.setState(({ open }) => ({
+      open: !open,
+    }))
+
+    dropdownClose = () => this.setState({ open: false })
+
+    render() {
+      // Disable outsideClickHandler when not running in a browser
+      const disableOutsideClickHandler = typeof document === 'undefined';
+
+      return (
+        <OutsideClickHandler
+          onOutsideClick={this.dropdownClose}
+          disabled={disableOutsideClickHandler}
+        >
+          <BaseComponent
+            {...this.props}
+            {...this.state}
+            toggleFilter={this.toggleFilter}
+            dropdownToggle={this.dropdownToggle}
+          />
+        </OutsideClickHandler>
+      );
+    }
   }
 
-  static propTypes = {
+  TableFiltersContainer.defaultProps = {
+    onChange: null,
+  };
+
+  TableFiltersContainer.propTypes = {
     /** Filter config */
     filters: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
 
     /** OnChange handler */
     onChange: PropTypes.func,
-  }
+  };
 
-  constructor(props) {
-    super(props);
-
-    const { filters } = this.props;
-
-    this.state = {
-      values: getInitialValues('', filters),
-      open: false,
-    };
-  }
-
-  toggleFilter = (key, value) => this.setState(({ values, ...state }) => {
-    const newState = {
-      ...state,
-      values: {
-        ...values,
-        [key]: value,
-      },
-    };
-
-    const { onChange } = this.props;
-
-    if (onChange) {
-      onChange(newState.values);
-    }
-
-    return newState;
-  })
-
-  dropdownToggle = () => this.setState(({ open }) => ({
-    open: !open,
-  }))
-
-  dropdownClose = () => this.setState({ open: false })
-
-  render() {
-    // Disable outsideClickHandler when not running in a browser
-    const disableOutsideClickHandler = typeof document === 'undefined';
-
-    return (
-      <OutsideClickHandler
-        onOutsideClick={this.dropdownClose}
-        disabled={disableOutsideClickHandler}
-      >
-        <BaseComponent
-          {...this.props}
-          {...this.state}
-          toggleFilter={this.toggleFilter}
-          dropdownToggle={this.dropdownToggle}
-        />
-      </OutsideClickHandler>
-    );
-  }
+  return TableFiltersContainer;
 };
