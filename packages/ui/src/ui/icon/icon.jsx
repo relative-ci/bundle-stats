d55f521b54b21e23869217d2ebadde5ab9483a19
Vio <vio@beanon.com>
2019-09-08 01:50:58 +0200

    refactor: Rename IconText -> Icon

---
+++ packages/ui/src/ui/icon/icon.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import BranchIcon from './assets/branch.svg';
+import ClockIcon from './assets/clock.svg';
+import CommitIcon from './assets/commit.svg';
+import FilterIcon from './assets/filter.svg';
+import PackageIcon from './assets/package.svg';
+import PullRequestIcon from './assets/pull-request.svg';
+import SortIcon from './assets/sort.svg';
+
+import css from './icon.module.css';
+
+const ICONS = {
+  branch: BranchIcon,
+  clock: ClockIcon,
+  commit: CommitIcon,
+  filter: FilterIcon,
+  package: PackageIcon,
+  pr: PullRequestIcon,
+  sort: SortIcon,
+};
+
+export const Icon = ({
+  className,
+  glyph,
+  as: Component,
+  ...restProps
+}) => {
+  const Svg = ICONS[glyph];
+
+  return (
+    <Component className={cx(css.root, className)} {...restProps}>
+      <Svg className={css.icon} />
+    </Component>
+  );
+};
+
+Icon.defaultProps = {
+  className: '',
+  as: 'span',
+};
+
+Icon.propTypes = {
+  className: PropTypes.string,
+  glyph: PropTypes.string.isRequired,
+  as: PropTypes.oneOfType([PropTypes.string, PropTypes.element, PropTypes.func]),
+};
