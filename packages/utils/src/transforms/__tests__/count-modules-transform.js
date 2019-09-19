0a56b0984bf752905bacaeba2cd2a60e0d49efda
Vio <vio@beanon.com>
2019-09-19 23:20:06 +0200

    refactor(utils): Extract countModulesTransform

---
+++ packages/utils/src/transforms/__tests__/count-modules-transform.js
@@ -0,0 +1,39 @@
+import { countModulesTransform } from '../count-modules-transform';
+
+describe('countModulesTransform', () => {
+  test('should return empty', () => {
+    const actual = countModulesTransform();
+    expect(actual).toEqual({ stats: { modulesCount: 0 } });
+  });
+
+  test('should return data', () => {
+    const actual = countModulesTransform({
+      modules: {
+        0: {
+          chunkNames: ['main'],
+          modules: {
+            'module-a': {
+              name: 'module-a',
+              value: 1000,
+            },
+            'module-b': {
+              name: 'module-b',
+              value: 2000,
+            },
+          },
+        },
+        1: {
+          chunkNames: ['vendors'],
+          modules: {
+            'module-c': {
+              name: 'module-c',
+              value: 3000,
+            },
+          },
+        },
+      },
+    });
+
+    expect(actual).toEqual({ stats: { modulesCount: 3 } });
+  });
+});
