0e0947f16e672d391468a173f9c70876ee719ee2
Vio <vio@beanon.com>
2019-09-22 18:26:10 +0200

    feat(ui): withCustomSort - add support for default sorting

--- packages/ui/src/hocs/with-custom-sort.js
+++ packages/ui/src/hocs/with-custom-sort.js
@@ -1,9 +1,11 @@
 import { orderBy } from 'lodash';
 import { compose, withProps, withState } from 'recompose';
 
-export const withCustomSort = ({ sortItems, getCustomSort, itemsKey }) => compose(
+export const withCustomSort = ({
+  sortItems, getCustomSort, itemsKey, sortBy = 'default', direction = 'asc',
+}) => compose(
   withProps({ sortItems }),
-  withState('sort', 'updateSort', { sortBy: 'default', direction: 'asc' }),
+  withState('sort', 'updateSort', { sortBy, direction }),
   withProps(({ sort, ...restProps }) => ({
     [itemsKey]: orderBy(restProps[itemsKey], getCustomSort(sort.sortBy), sort.direction),
   })),
