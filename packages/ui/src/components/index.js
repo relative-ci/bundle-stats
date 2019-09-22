aafce6bd0963891cc240f61d987edd0d9a07630d
Vio <vio@beanon.com>
2019-09-22 18:45:14 +0200

    fixup ui bundle packages

--- packages/ui/src/components/index.js
+++ packages/ui/src/components/index.js
@@ -3,6 +3,7 @@ export { BundleAssets } from './bundle-assets';
 export { BundleAssetsTotalsTable } from './bundle-assets-totals-table';
 export { BundleAssetsTotalsChartBars } from './bundle-assets-totals-chart-bars';
 export { BundleChunkModules } from './bundle-chunk-modules';
+export { BundlePackages } from './bundle-packages';
 export { BundleModules } from './bundle-modules';
 export { Delta } from './delta';
 export { JobName } from './job-name';
