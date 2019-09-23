8bcc57c26e838c7b389488ceaf5f08221e856f30
Vio <vio@beanon.com>
2019-09-24 00:35:22 +0200

    fix(utils): getMetricType - prevent partial match

--- packages/utils/src/metrics/__tests__/get-metric-type.js
+++ packages/utils/src/metrics/__tests__/get-metric-type.js
@@ -1,11 +1,30 @@
 import { omit } from 'lodash';
 
+import { METRIC_TYPE_FILE_SIZE } from '../../config/metrics';
 import { getMetricType } from '../get-metric-type';
 
-test('getMetric', () => {
-  expect(omit(getMetricType('webpack.assets.totalSizeByTypeALL'), ['formatter'])).toEqual(omit({
-    label: 'Total Size',
-    type: 'METRIC_TYPE_FILE_SIZE',
-    biggerIsBetter: false,
-  }, ['formatter']));
+describe('getMetricType', () => {
+  test('should return metric', () => {
+    expect(omit(getMetricType('webpack.assets.totalSizeByTypeALL'), ['formatter'])).toEqual(omit({
+      label: 'Total Size',
+      type: 'METRIC_TYPE_FILE_SIZE',
+      biggerIsBetter: false,
+    }, ['formatter']));
+  });
+
+  test('should return fallback metric for partial matches', () => {
+    expect(omit(getMetricType('webpack', METRIC_TYPE_FILE_SIZE), ['formatter'])).toEqual(omit({
+      label: 'webpack',
+      type: 'METRIC_TYPE_FILE_SIZE',
+      biggerIsBetter: false,
+    }, ['formatter']));
+  });
+
+  test('should return fallback metric', () => {
+    expect(omit(getMetricType('/assets/main.js', METRIC_TYPE_FILE_SIZE), ['formatter'])).toEqual(omit({
+      label: '/assets/main.js',
+      type: 'METRIC_TYPE_FILE_SIZE',
+      biggerIsBetter: false,
+    }, ['formatter']));
+  });
 });
--- packages/utils/src/metrics/get-metric-type.js
+++ packages/utils/src/metrics/get-metric-type.js
@@ -3,13 +3,20 @@ import { get } from 'lodash';
 import METRICS, { METRIC_TYPE_NUMERIC, METRIC_TYPES } from '../config/metrics';
 
 export const getMetricType = (key, type) => {
-  const metric = get(METRICS, key, {
-    label: key,
-    type: type || METRIC_TYPE_NUMERIC,
-  });
+  const metric = get(METRICS, key);
+
+  if (metric && metric.type) {
+    return {
+      ...METRIC_TYPES[metric.type],
+      ...metric,
+    };
+  }
+
+  const resolvedType = type || METRIC_TYPE_NUMERIC;
 
   return {
-    ...METRIC_TYPES[metric.type],
-    ...metric,
+    ...METRIC_TYPES[resolvedType],
+    type: resolvedType,
+    label: key,
   };
 };
