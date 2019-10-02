e2608b52a6e90440e1ba6fbf5fcd2d61101f185c
Vio <vio@beanon.com>
2019-10-03 00:06:56 +0200

    fix: Remove preact-compat

--- build/configs/resolve.js
+++ build/configs/resolve.js
@@ -4,8 +4,8 @@ module.exports = () => ({
   resolve: {
     extensions: ['.jsx', '.json', '.js'],
     alias: {
-      react: 'preact-compat',
-      'react-dom': 'preact-compat',
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
       Fixtures: path.join(__dirname, '../../fixtures'),
     },
   },
