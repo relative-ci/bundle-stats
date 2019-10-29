1d56500566b72a0337e6a75835458370f9eacb20
Vio <vio@beanon.com>
2019-10-29 18:03:53 +0100

    refactor(html-templates): Move storybook inside build folder

---
+++ packages/html-templates/build/storybook/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpackMerge = require('webpack-merge');
+
+const getCssConfig = require('../../../../build/configs/css');
+const getDefineConfig = require('../../../../build/configs/define');
+const getResolveConfig = require('../../../../build/configs/resolve');
+const getFilesConfig = require('../../../../build/configs/files');
+const settings = require('../../settings');
+
+module.exports = ({ config }) => {
+  // Remove other rules than js & md
+  config.module.rules = config.module.rules.slice(0, 2); // eslint-disable-line no-param-reassign
+
+  return webpackMerge.smart(
+    config,
+    getCssConfig(settings),
+    getDefineConfig(settings),
+    getFilesConfig(settings),
+    getResolveConfig(settings),
+    {
+      resolve: {
+        modules: [
+          // Resolve dependencies to other @bundle-stats packages when linked
+          path.join(__dirname, '../../node_modules'),
+          path.join(__dirname, '../../ui/node_modules'),
+          path.join(__dirname, '../../utils/node_modules'),
+        ],
+      },
+    },
+  );
+};
