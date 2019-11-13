f68e9f1de91d1e58acb2e3116647c55f3922e4f5
Vio <vio@beanon.com>
2019-11-13 23:16:24 +0100

    fix(ui): SummaryItem - fix delta alignment

--- packages/ui/src/components/jobs-header/jobs-header.stories.jsx
+++ packages/ui/src/components/jobs-header/jobs-header.stories.jsx
@@ -72,6 +72,47 @@ stories.add('multiple jobs', () => (
   />
 ));
 
+stories.add('multiple equal jobs', () => (
+  <JobsHeader
+    jobs={[
+      {
+        internalBuildNumber: 1,
+        meta: {
+          webpack: {
+            stats: {
+              builtAt: '2019-01-01T00:00:00.000Z',
+              hash: 'abcd1234',
+            },
+          },
+        },
+        summary: {
+          'webpack.assets.totalSizeByTypeALL': {
+            current: 1000000,
+            baseline: 1000000,
+          },
+        },
+      },
+      {
+        internalBuildNumber: 2,
+        meta: {
+          webpack: {
+            stats: {
+              builtAt: '2019-01-02T00:00:00.000Z',
+              hash: 'efgh1234',
+            },
+          },
+        },
+        summary: {
+          'webpack.assets.totalSizeByTypeALL': {
+            current: 1000000,
+            baseline: 0,
+          },
+        },
+      },
+    ]}
+  />
+));
+
 stories.add('loading', () => (
   <JobsHeader loading />
 ));
