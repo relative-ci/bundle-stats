3be49c052c33ac2105e9f484e2134c5ac1f143e2
Vio <vio@beanon.com>
2020-01-12 11:07:18 +0100

    refactor(utils): Move selectors inside webpack

---
+++ packages/utils/src/webpack/constants.js
@@ -0,0 +1,11 @@
+export const SUMMARY_METRIC_PATHS = [
+  'totalSizeByTypeALL',
+  'totalInitialSizeJS',
+  'totalInitialSizeCSS',
+  'cacheInvalidation',
+  'moduleCount',
+  'chunkCount',
+  'assetCount',
+  'packageCount',
+  'duplicatePackagesCount',
+];
