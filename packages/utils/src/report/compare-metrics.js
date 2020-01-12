26ff1c17255f0e31747cc9f974f026deabb435a8
Vio <vio@beanon.com>
2020-01-12 20:57:27 +0100

    refactor(utils): mergeRunsById -> mergeMetricsByKey

--- packages/utils/src/report/compare-metrics.js
+++ packages/utils/src/report/compare-metrics.js
@@ -1,11 +1,11 @@
 import { map } from 'lodash';
 
 import { addRowData } from './add-row-data';
-import { mergeRunsById } from './merge-runs-by-id';
+import { mergeMetricsByKey } from './merge-metrics-by-key';
 
 export const compareMetrics = (jobs, selectMetrics, metricType) => {
   const data = map(jobs, selectMetrics);
-  const rows = mergeRunsById(data);
+  const rows = mergeMetricsByKey(data);
 
   return rows.map((row) => addRowData(row, metricType));
 };
