e70e76372106913c7b0cc4954902f79ddf3fb579
Vio <vio@beanon.com>
2020-01-13 23:15:26 +0100

    feat(utils): Add getMetricRunInfo

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -2,6 +2,5 @@ export * from './browsertime';
 export * from './config';
 export * from './jobs';
 export * from './lighthouse';
-export * from './metrics';
 export * from './report';
 export * from './utils';
