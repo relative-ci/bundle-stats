da8ec09f558a8b1cbb1e6d78d8cd0d516e48374b
Vio <vio@beanon.com>
2020-01-15 23:07:38 +0100

    refactor(cli-utils): Rename createReports to createArtifacts

--- packages/cli-utils/src/index.js
+++ packages/cli-utils/src/index.js
@@ -1,6 +1,6 @@
 import * as T from './text';
 
 export * from './baseline';
-export * from './create-report';
+export * from './create-artifacts';
 export * from './constants';
 export const TEXT = T;
