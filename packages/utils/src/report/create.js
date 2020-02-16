80eb05e3fe39d404d4795a1c69c654dfba3a2659
Vio <vio@beanon.com>
2020-02-16 23:00:03 +0100

    feature(utils): Report - show summary section

--- packages/utils/src/report/create.js
+++ packages/utils/src/report/create.js
@@ -1,15 +1,39 @@
 import { get, isEmpty } from 'lodash';
 
+import { SOURCE_PATHS } from '../config';
+import { getMetricType, getMetricRunInfo } from '../utils/metrics';
 import * as webpack from '../webpack';
 
 export const createReport = (jobs) => {
   const insights = get(jobs, '[0].insights');
+
+  const summary = SOURCE_PATHS.reduce((agg, sourceId) => {
+    const sourceSummary = get(jobs, [0, 'summary', sourceId]);
+
+    const output = Object.entries(sourceSummary).map(([metricId, summaryData]) => {
+      const metric = getMetricType(`${sourceId}.${metricId}`);
+      const { current = 0, baseline = 0 } = summaryData;
+      const info = getMetricRunInfo(metric, current, baseline);
+
+      return {
+        label: metric.label,
+        ...info,
+      };
+    });
+
+    return {
+      ...agg,
+      [sourceId]: output,
+    };
+  }, {});
 
   return {
     runs: jobs.map(({ internalBuildNumber, meta }) => ({
       ...meta,
       internalBuildNumber,
     })),
+
+    summary,
 
     // Add insights if available
     ...!isEmpty(insights) ? { insights } : {},
