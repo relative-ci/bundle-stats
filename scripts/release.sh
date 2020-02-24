b3f076dcd90e19e9ae4a2db886c88c290625fd07
Vio <vio@beanon.com>
2020-02-24 23:25:04 +0100

    build: Auto release on tag push

---
+++ scripts/release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const { execSync } = require('child_process');
+
+const tag = execSync('git tag -l --points-at HEAD')
+  .toString()
+  .trim();
+
+const matchTag = tag.match(/^v\d*\.\d*\.\d*-(\w*)\..*$/);
+const distTag = (matchTag && matchTag[1]) || 'latest';
+
+const options = [
+  'publish',
+  'from-git',
+  `--dist-tag ${distTag}`,
+  '--no-changelog',
+  '--no-git-tag-version',
+  '--no-push',
+  '--yes'
+];
+
+console.log(`Running lerna with "${options.join(' ')}"`);
+
+execSync(`npm run lerna -- ${options.join(' ')}`);
