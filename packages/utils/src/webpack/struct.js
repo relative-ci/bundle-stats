c2f2ba670fd8bdc2b8b5b60ede55f357dd07399a
Vio <vio@beanon.com>
2020-04-08 00:25:35 +0200

    fix(utils): Validate webpack stats structure

---
+++ packages/utils/src/webpack/struct.js
@@ -0,0 +1,34 @@
+import { superstruct } from 'superstruct';
+
+const struct = superstruct({
+  types: {
+    notEmptyArray: (value) => value?.length > 0,
+  },
+});
+
+export const WebpackSourceAssetStruct = struct.interface({
+  name: 'string',
+  size: 'number',
+});
+
+export const WebpackSourceModuleChunk = struct.interface({
+  name: 'string',
+  size: 'number',
+  chunks: [struct.union(['number', 'string'])],
+});
+
+export const WebpackSourceChunkStruct = struct.interface({
+  id: struct.union(['number', 'string']),
+  entry: 'boolean',
+  initial: 'boolean',
+  names: ['string'],
+  files: ['string'],
+});
+
+export const WebpackSourceStruct = struct.interface({
+  hash: struct.optional('string'),
+  builtAt: struct.optional('number'),
+  assets: struct.intersection([[WebpackSourceAssetStruct], 'notEmptyArray']),
+  modules: struct.optional([WebpackSourceModuleChunk]),
+  chunks: struct.optional([WebpackSourceChunkStruct]),
+});
