a6be8dba3f30b2a0e1480e5aa19a31b398184869
Vio <vio@beanon.com>
2020-04-21 07:45:03 +0200

    fix(utils): Webpack - shorten metric labels

--- packages/utils/src/utils/__tests__/get-global-metric-type.js
+++ packages/utils/src/utils/__tests__/get-global-metric-type.js
@@ -5,33 +5,55 @@ import { getGlobalMetricType } from '../metrics';
 
 describe('getGlobalMetricType', () => {
   test('should return metric', () => {
-    expect(omit(getGlobalMetricType('webpack.totalSizeByTypeALL'), ['formatter'])).toEqual(omit({
-      label: 'Total Bundle Size',
-      description: 'Sum of all assets.',
-      type: 'METRIC_TYPE_FILE_SIZE',
-      biggerIsBetter: false,
-    }, ['formatter']));
+    expect(omit(getGlobalMetricType('webpack.totalSizeByTypeALL'), ['formatter'])).toEqual(
+      omit(
+        {
+          label: 'Bundle Size',
+          description: 'Sum of all assets.',
+          type: 'METRIC_TYPE_FILE_SIZE',
+          biggerIsBetter: false,
+        },
+        ['formatter'],
+      ),
+    );
 
-    expect(omit(getGlobalMetricType('webpack.sizes.totalSizeByTypeJS'), ['formatter'])).toEqual(omit({
-      label: 'JS',
-      type: 'METRIC_TYPE_FILE_SIZE',
-      biggerIsBetter: false,
-    }, ['formatter']));
+    expect(omit(getGlobalMetricType('webpack.sizes.totalSizeByTypeJS'), ['formatter'])).toEqual(
+      omit(
+        {
+          label: 'JS',
+          type: 'METRIC_TYPE_FILE_SIZE',
+          biggerIsBetter: false,
+        },
+        ['formatter'],
+      ),
+    );
   });
 
   test('should return fallback metric for partial matches', () => {
-    expect(omit(getGlobalMetricType('webpack', METRIC_TYPE_FILE_SIZE), ['formatter'])).toEqual(omit({
-      label: 'webpack',
-      type: 'METRIC_TYPE_FILE_SIZE',
-      biggerIsBetter: false,
-    }, ['formatter']));
+    expect(omit(getGlobalMetricType('webpack', METRIC_TYPE_FILE_SIZE), ['formatter'])).toEqual(
+      omit(
+        {
+          label: 'webpack',
+          type: 'METRIC_TYPE_FILE_SIZE',
+          biggerIsBetter: false,
+        },
+        ['formatter'],
+      ),
+    );
   });
 
   test('should return fallback metric', () => {
-    expect(omit(getGlobalMetricType('/assets/main.js', METRIC_TYPE_FILE_SIZE), ['formatter'])).toEqual(omit({
-      label: '/assets/main.js',
-      type: 'METRIC_TYPE_FILE_SIZE',
-      biggerIsBetter: false,
-    }, ['formatter']));
+    expect(
+      omit(getGlobalMetricType('/assets/main.js', METRIC_TYPE_FILE_SIZE), ['formatter']),
+    ).toEqual(
+      omit(
+        {
+          label: '/assets/main.js',
+          type: 'METRIC_TYPE_FILE_SIZE',
+          biggerIsBetter: false,
+        },
+        ['formatter'],
+      ),
+    );
   });
 });
