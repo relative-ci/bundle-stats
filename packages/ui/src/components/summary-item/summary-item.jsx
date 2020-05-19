61146381b1049a04a3bc295647140bd8163ea79d
Vio <vio@beanon.com>
2020-05-19 23:14:12 +0200

    feat(ui): SummaryItem - add inline variation, show description on title

--- packages/ui/src/components/summary-item/summary-item.jsx
+++ packages/ui/src/components/summary-item/summary-item.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { getGlobalMetricType, getMetricRunInfo } from '@bundle-stats/utils';
 
-import { Icon, Tooltip } from '../../ui';
+import { Tooltip } from '../../ui';
 import { Metric } from '../metric';
 import { Delta } from '../delta';
 import css from './summary-item.module.css';
@@ -16,35 +16,38 @@ export const SummaryItem = ({
   loading,
   showDelta,
   showMetricDescription,
+  inline,
 }) => {
   const { baseline, current } = data || { baseline: 0, current: 0 };
 
   const metric = getGlobalMetricType(id);
   const runInfo = getMetricRunInfo(metric, current, baseline);
+  const showMetricDescriptionTooltip = showMetricDescription && metric?.description;
 
-  const rootClassName = cx(css.root, className, css[size]);
+  const rootClassName = cx(
+    css.root,
+    className,
+    css[size],
+    inline && css.inline,
+    showMetricDescriptionTooltip && css.showMetricDescription,
+  );
 
   return (
     <div className={rootClassName}>
-      <h3 className={css.title}>
-        <span>{metric.label}</span>
-
-        {showMetricDescription && metric.description && (
-          <Tooltip
-            as="button"
-            type="button"
-            className={css.helpButton}
-            title={
-              <div className={css.helpTooltip}>
-                <h4 className={css.helpTooltipTitle}>{metric.label}</h4>
-                <p className={css.helpTooltipDescription}>{metric.description}</p>
-              </div>
-            }
-          >
-            <Icon glyph="help" className={css.helpButtonIcon} />
-          </Tooltip>
-        )}
-      </h3>
+      <Tooltip
+        as="h3"
+        className={css.title}
+        title={
+          showMetricDescriptionTooltip && (
+            <div className={css.helpTooltip}>
+              <h4 className={css.helpTooltipTitle}>{metric.label}</h4>
+              <p className={css.helpTooltipDescription}>{metric.description}</p>
+            </div>
+          )
+        }
+      >
+        {metric.label}
+      </Tooltip>
 
       {!loading ? (
         <Metric
@@ -79,6 +82,7 @@ SummaryItem.defaultProps = {
   loading: false,
   showMetricDescription: false,
   showDelta: true,
+  inline: false,
 };
 
 SummaryItem.propTypes = {
@@ -102,4 +106,7 @@ SummaryItem.propTypes = {
 
   /** Show delta */
   showDelta: PropTypes.bool,
+
+  /** Inline flag */
+  inline: PropTypes.bool,
 };
