ea1e5900fda85af735cc01764913d9402f594c88
Vio <vio@beanon.com>
2020-09-30 18:20:00 +0200

    test: Mock date

--- packages/webpack-plugin/test/package/webpack-plugin.test.js
+++ packages/webpack-plugin/test/package/webpack-plugin.test.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack');
 const MemoryFS = require('memory-fs');
+const { advanceTo } = require('jest-date-mock');
 const config = require('./app/webpack.config');
+
+advanceTo(new Date(2020, 10, 30));
 
 jest.setTimeout(10 * 1000);
 
@@ -8,7 +11,7 @@ test('webpack plugin package', (done) => {
   expect.assertions(3);
 
   const compiler = webpack(config);
-  compiler.outputFileSystem = new MemoryFS()
+  compiler.outputFileSystem = new MemoryFS();
 
   compiler.run((error, stats) => {
     expect(error).toEqual(null);
