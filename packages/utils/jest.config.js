16561ea075b6dbe487a2d59308147d4c986f8a4f
Vio <vio@beanon.com>
2021-04-02 23:45:31 +0200

    build(utils): Setup typescript

--- packages/utils/jest.config.js
+++ packages/utils/jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+  preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
-  setupFiles: [
-    'core-js',
-  ],
+  setupFiles: ['core-js'],
   testPathIgnorePatterns: [
+    '/__fixtures__/',
     '/node_modules/',
     '/lib/',
     '/lib-esm/',
+    '/types/',
   ],
 };
