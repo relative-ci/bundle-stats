e3d0fcd2f53384ab0a843642995549157545449e
Vio <vio@beanon.com>
2021-04-18 10:07:20 +0200

    fix(ui): JobName - allow children

--- packages/ui/src/components/job-name/job-name.jsx
+++ packages/ui/src/components/job-name/job-name.jsx
@@ -4,54 +4,17 @@ import PropTypes from 'prop-types';
 import { Tooltip } from '../../ui/tooltip';
 import css from './job-name.module.css';
 
-const JobNameLabel = ({ internalBuildNumber, labelProps }) => (
-  <>
-    <span {...labelProps}>
-      Job
-    </span>
-    <span>
-      {`#${internalBuildNumber}`}
-    </span>
-  </>
-);
-
-JobNameLabel.defaultProps = {
-  labelProps: {},
-};
-
-JobNameLabel.propTypes = {
-  internalBuildNumber: PropTypes.number.isRequired,
-  labelProps: PropTypes.shape({
-    className: PropTypes.string,
-  }),
-};
-
-export const JobName = ({
-  as: Component,
-  title,
-  internalBuildNumber,
-  render,
-  ...restProps
-}) => (
-  <Tooltip
-    className={css.root}
-    as={Component}
-    title={title}
-    {...restProps}
-  >
-    {render({
-      internalBuildNumber,
-      labelProps: {
-        className: css.label,
-      },
-    })}
+export const JobName = ({ as: Component, title, internalBuildNumber, children, ...restProps }) => (
+  <Tooltip className={css.root} as={Component} title={title} {...restProps}>
+    {children || `Job #${internalBuildNumber}`}
   </Tooltip>
 );
 
 JobName.defaultProps = {
   as: 'span',
   title: '',
-  render: JobNameLabel,
+  internalBuildNumber: null,
+  children: null,
 };
 
 JobName.propTypes = {
@@ -62,8 +25,8 @@ JobName.propTypes = {
   title: PropTypes.string,
 
   /** Job internal build number */
-  internalBuildNumber: PropTypes.number.isRequired,
+  internalBuildNumber: PropTypes.number,
 
   /** Render function */
-  render: PropTypes.func,
+  children: PropTypes.node,
 };
