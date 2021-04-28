cfad9526a7133b1850f276f57abc06dc3c2cc98b
Vio <vio@beanon.com>
2021-04-28 22:43:41 +0200

    feat(utils): Webpack - extract concatenated modules

---
+++ packages/utils/__fixtures__/webpack-stats-1-concatenated.extracted.js
@@ -0,0 +1,87 @@
+module.exports = {
+  builtAt: 1546300800000,
+  hash: 'abcd1234',
+  assets: [
+    {
+      name: 'main.a1b2c3d.css',
+      size: 10000,
+    },
+    {
+      name: 'main.a2b3c4d.js',
+      size: 50000,
+    },
+    {
+      name: 'logo.a3b4c5d.png',
+      size: 1000,
+    },
+    {
+      name: 'index.html',
+      size: 2000,
+    },
+  ],
+  chunks: [
+    {
+      entry: true,
+      id: 1,
+      initial: true,
+      files: ['main.a1b2c3d.css', 'main.a2b3c4d.js'],
+      names: ['main'],
+    },
+  ],
+  entrypoints: {
+    main: {
+      assets: [
+        'main.a1b2c3d.css',
+        'main.a1b2c3d.css.map',
+        'main.a2b3c4d.js',
+        'main.a2b3c4d.js.map',
+      ],
+    },
+  },
+  modules: [
+    {
+      chunks: [1],
+      name: 'module-a',
+      size: 1000,
+    },
+    {
+      chunks: [1],
+      name: './module-b + 2 concatenated modules',
+      size: 2000,
+      modules: [
+        {
+          name: './module-b',
+          size: 500,
+        },
+        {
+          name: './module-b-1',
+          size: 1000,
+        },
+        {
+          name: './module-b-2',
+          size: 500,
+        },
+      ],
+    },
+    {
+      chunks: [1],
+      name: 'node_modules/package-a/index.js',
+      size: 1000,
+    },
+    {
+      chunks: [1],
+      name: 'node_modules/package-a/node_modules/package-c/index.js',
+      size: 1000,
+    },
+    {
+      chunks: [1],
+      name: 'node_modules/package-b/index.js',
+      size: 1000,
+    },
+    {
+      chunks: [1],
+      name: 'node_modules/package-c/index.js',
+      size: 1000,
+    },
+  ],
+};
