812770b763cd1b07dbbe174d467cec3aa0694aa6
Vio <vio@beanon.com>
2021-05-22 23:28:42 +0200

    feat(ui): Icon - add size modifier

--- packages/ui/src/ui/icon/icon.stories.jsx
+++ packages/ui/src/ui/icon/icon.stories.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { getWrapperDecorator } from '../../stories';
-import { Icon, ICONS } from '.';
+import { Icon } from '.';
 
 const stories = storiesOf('UI/Icon', module);
 stories.addDecorator(getWrapperDecorator());
 
-stories.add('default', () => <Icon glyph={ICONS.ARROW} />);
+stories.add('default', () => <Icon glyph={Icon.ICONS.ARROW} />);
 
-stories.add('all', () =>
-  Object.values(ICONS).map((glyph) => <Icon glyph={glyph} key={glyph} />),
-);
+stories.add('with size', () => <Icon glyph={Icon.ICONS.ARROW} size={Icon.SIZE_LARGE} />);
+
+stories.add('all', () => (
+  <>
+    {Object.values(Icon.ICONS).map((glyph) => (
+      <span style={{ margin: '0.3rem' }}>
+        <Icon glyph={glyph} key={glyph} />
+      </span>
+    ))}
+  </>
+));
