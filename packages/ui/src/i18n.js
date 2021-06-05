a5a55abf321d95fdf4a9550b49dda71dc6bff72a
Vio <vio@beanon.com>
2021-06-05 14:46:58 +0200

    fix(ui): Reword module/package size information

--- packages/ui/src/i18n.js
+++ packages/ui/src/i18n.js
@@ -14,8 +14,8 @@ export default {
 
   ASSETS_INFO: 'Asset size is the disk file size',
   ASSET_TOTALS_INFO: 'Total file type size is the disk size of the corresponding assets.',
-  MODULES_INFO: 'Module size is the estimated size before any production optimization (eg: minimization).',
-  PACKAGES_INFO: 'Package size is the estimated size of the contained modules before any production optimization(eg: minification).',
+  MODULES_INFO: 'Module size is the estimated size (stat size) before any production optimization (eg: minification).',
+  PACKAGES_INFO: 'Package size is the estimated size (stat size) of the contained modules before any production optimization(eg: minification).',
 
   RESET_FILTERS: 'Reset filters',
   VIEW_ALL: 'View all entries',
