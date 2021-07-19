9b7111cd5e5137a875525d54948c6a7e48795023
Vio <vio@beanon.com>
2021-07-19 20:09:11 +0200

    fix(utils): ModuleName - replace invalid css prefix

--- packages/utils/src/webpack/__tests__/utils-get-module-name.js
+++ packages/utils/src/webpack/__tests__/utils-get-module-name.js
@@ -4,24 +4,31 @@ describe('Webpack/utils/getModuleName', () => {
   test('should return empty name if missing', () => {
     expect(getModuleName()).toBe('');
   });
+
   test('should return name as it is', () => {
     expect(getModuleName('./node_modules/lodash/_apply.js')).toBe(
       './node_modules/lodash/_apply.js',
     );
   });
 
-  test('should skip loader details', () => {
+  test('should remove loader details', () => {
     expect(getModuleName('!babel-loader!eslint-loader!./node_modules/lodash/_apply.js')).toBe(
       './node_modules/lodash/_apply.js',
     );
     expect(getModuleName('plugin/src/loader.js?{"modules":["./src/main.js"]}!')).toBe(
-      'plugin/src/loader.js?{"modules":["./src/main.js"]}!'
+      'plugin/src/loader.js?{"modules":["./src/main.js"]}!',
     );
   });
 
-  test('should skip webpack module details', () => {
+  test('should remove webpack module details', () => {
     expect(getModuleName('./node_modules/lodash/_apply.js + 7 modules')).toBe(
       './node_modules/lodash/_apply.js',
+    );
+  });
+
+  test('should remove invalid node_modules prefix', () => {
+    expect(getModuleName('css ../node_modules../node_modules/package-a/style.css')).toBe(
+      '../node_modules/package-a/style.css',
     );
   });
 });
