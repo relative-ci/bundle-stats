29c86067d07188e4a553f7a41f70cc0621ea7c0d
Vio <vio@beanon.com>
2021-10-09 01:04:38 +0200

    fix(utils): Webpack module packages -construct patterns

--- packages/utils/src/webpack/constants.js
+++ packages/utils/src/webpack/constants.js
@@ -24,12 +24,67 @@ export const SUMMARY_METRIC_PATHS = [
   'duplicatePackagesCount',
 ];
 
+const NAME_SLUG = /[\w|\-|_|.]+/;
+const VERSION = /@[\w|\-|_|.]+/;
+
 // Extract package paths from module path
 // https://regex101.com/r/22Leep/5
-export const MODULE_PATH_PACKAGES =
-  /((?:node_modules|~)(?:\/\.pnpm)?\/)((?:@[\w|\-|_|.]*[/|+])?(?:[\w|\-|_|.]+\+)*(?:[\w|\-|_|.]*(?:@[\w|\-|_|.]*)?)(?:_(?:@[\w|\-|_|.]*[/|+])?(?:[\w|\-|_|.]*(?:@[\w|\-|_|.]*)?))*\/)/g
+/* eslint-disable prettier/prettier */
+export const MODULE_PATH_PACKAGES = new RegExp([
+  // match dependency directory (eg: `node_modules/`, `node_modules/.pnpm/`)
+  '(',
+    '(?:node_modules|~)',
+    '(?:/\\.pnpm)?',
+    '/',
+  ')',
+  // match package name
+  '(',
+    // match `@organization/` or `@organization+`(pnpm)
+    `(?:@${NAME_SLUG.source}[/|+])?`,
+    // match github.com+organization+
+    `(?:${NAME_SLUG.source}\\+)*`,
+    // match package name
+    `(?:${NAME_SLUG.source})`,
+    // match version
+    `(?:${VERSION.source})?`,
+  ')',
+
+  // Match pnpm peer dependencies (eg: package-a@version_package-b@version)
+  '(?:_',
+    `(?:@${NAME_SLUG.source}[/|+])?`,
+    `(?:${NAME_SLUG.source})`,
+    `(?:@${NAME_SLUG.source})?`,
+  ')*',
+  '/',
+].join(''), 'g');
+/* eslint-enable prettier/prettier */
 
 // Extract package name from package path
 // https://regex101.com/r/tTlU0W/5
-export const PACKAGE_PATH_NAME =
-  /(?:(?:node_modules|~)(?:\/\.pnpm)?)\/(?:((?:@[\w|\-|_|.]*[/|+])?(?:(?:[\w|\-|_|.]+\+)*)(?:[\w|\-|_|.]+))(?:@[\w|\-|_|.]+)?)(?:_(?:(?:((?:@[\w|\-|_|.]+[/|+])?(?:[\w|\-|_|.]+))(?:@[\w|\-|_|.]*))))*\//g;
+/* eslint-disable prettier/prettier */
+export const PACKAGE_PATH_NAME = new RegExp([
+  '(?:',
+    '(?:node_modules|~)',
+    '(?:/\\.pnpm)?',
+  ')',
+  '/',
+  // match dependency
+  '(?:',
+    '(',
+      `(?:@${NAME_SLUG.source}[/|+])?`,
+      `(?:(?:${NAME_SLUG.source}\\+)*)`,
+      `(?:${NAME_SLUG.source})`,
+    ')',
+    `(?:${VERSION.source})?`,
+  ')',
+  // match peer dependency
+  '(?:_',
+    '(',
+      `(?:@${NAME_SLUG.source}[/|+])?`,
+      `(?:${NAME_SLUG.source})`,
+    ')',
+    `(?:@${NAME_SLUG.source})`,
+  ')*',
+  '/',
+].join(''), 'g');
+/* eslint-enable prettier/prettier */
