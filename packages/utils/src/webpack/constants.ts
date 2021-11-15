97dc816da9db74fee5b7232527aa2e00b1b5d962
Vio <vio@beanon.com>
2021-11-15 22:05:16 +0100

    refactor(utils): Webpack metrics - use enums for metrics

--- packages/utils/src/webpack/constants.ts
+++ packages/utils/src/webpack/constants.ts
@@ -12,17 +12,32 @@ export const SECTIONS = [
   SECTION_WEBPACK_PACKAGES,
 ];
 
-export const SUMMARY_METRIC_PATHS = [
-  'totalSizeByTypeALL',
-  'totalInitialSizeJS',
-  'totalInitialSizeCSS',
-  'cacheInvalidation',
-  'moduleCount',
-  'chunkCount',
-  'assetCount',
-  'packageCount',
-  'duplicatePackagesCount',
-];
+export enum MetricId {
+  BundleSize = 'totalSizeByTypeALL',
+  TotalInitialSizeJS = 'totalInitialSizeJS',
+  TotalInitialSizeCSS = 'totalInitialSizeCSS',
+  CacheInvalidation = 'cacheInvalidation',
+  ModuleName = 'moduleCount',
+  ChunkCount = 'chunkCount',
+  AssetCount = 'assetCount',
+  PackageCount = 'packageCount',
+  DuplicatePackagesCount = 'duplicatePackagesCount',
+}
+
+export const SIZE_BY_TYPE_METRIC_ID_ALL = 'ALL';
+export const SIZE_BY_TYPE_METRIC_ID_PREFIX = 'totalSizeByType';
+
+export enum SizeByTypeMetricId {
+  JS = 'totalSizeByTypeJS',
+  CSS = 'totalSizeByTypeCSS',
+  IMG = 'totalSizeByTypeIMG',
+  Media = 'totalSizeByTypeMEDIA',
+  Font = 'totalSizeByTypeFONT',
+  HTML = 'totalSizeByTypeHTML',
+  Other = 'totalSizeByTypeOTHER',
+}
+
+export const SUMMARY_METRIC_PATHS = Object.values(MetricId);
 
 const PACKAGE_PREFIX = /(?:node_modules|~)(?:\/\.pnpm)?/;
 const PACKAGE_SLUG = /[a-zA-Z0-9]+(?:[-|_|.]+[a-zA-Z0-9]+)*/;
@@ -30,7 +45,7 @@ const VERSION = /@[\w|\-|_|.]+/;
 
 // Extract package paths from module path
 // https://regex101.com/r/22Leep/6
-/* eslint-disable prettier/prettier */
+/* eslint-disable prettier/prettier, @typescript-eslint/indent */
 export const MODULE_PATH_PACKAGES = new RegExp([
   // match dependency directory (eg: `node_modules/`, `node_modules/.pnpm/`)
   `(?:${PACKAGE_PREFIX.source}/)`,
@@ -54,11 +69,9 @@ export const MODULE_PATH_PACKAGES = new RegExp([
   ')*',
   '/',
 ].join(''), 'g');
-/* eslint-enable prettier/prettier */
 
 // Extract package name from package path
 // https://regex101.com/r/tTlU0W/6
-/* eslint-disable prettier/prettier */
 export const PACKAGE_PATH_NAME = new RegExp([
   `(?:${PACKAGE_PREFIX.source}/)`,
   // match dependency
@@ -80,4 +93,4 @@ export const PACKAGE_PATH_NAME = new RegExp([
   ')*',
   '/',
 ].join(''), 'g');
-/* eslint-enable prettier/prettier */
+/* eslint-enable prettier/prettier, @typescript-eslint/indent */
