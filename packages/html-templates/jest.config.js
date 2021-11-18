3d249b6a90814d253e4130cc277149c0dafa0ddb
Vio <vio@beanon.com>
2021-11-18 22:35:49 +0100

    test: Run jest tests on jsdom

--- packages/html-templates/jest.config.js
+++ packages/html-templates/jest.config.js
@@ -4,15 +4,11 @@ module.exports = {
     __VERSION__: '0.0.1',
   },
   globalSetup: '<rootDir>/build/jest/global-setup.js',
-  setupFiles: [
-    'core-js',
-    'jest-date-mock',
-    '<rootDir>/build/jest/register-context.js',
+  setupFiles: ['core-js', 'jest-date-mock', '<rootDir>/build/jest/register-context.js'
   ],
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
     'Fixtures/(.*)$': '<rootDir>/../../fixtures/$1',
     '@bundle-stats/(.*)/lib-esm/(.*)': '@bundle-stats/$1/lib/$2',
   },
-  testEnvironment: 'node',
 };
