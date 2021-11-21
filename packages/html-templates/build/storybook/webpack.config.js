acd365048859452ee231b74d994ad40b4fe912b3
Vio <vio@beanon.com>
2021-11-21 15:42:55 +0100

    build(html-templates): Upgrade storybook setup

--- packages/html-templates/build/storybook/webpack.config.js
+++ packages/html-templates/build/storybook/webpack.config.js
@@ -3,27 +3,25 @@ const { merge } = require('webpack-merge');
 
 const getDefineConfig = require('../../../../build/configs/define');
 const getResolveConfig = require('../../../../build/configs/resolve');
-const getFilesConfig = require('../../../../build/configs/files');
 const settings = require('../../settings');
 
 module.exports = ({ config }) => {
-  // Remove other rules than js & md
-  config.module.rules = config.module.rules.slice(0, 2); // eslint-disable-line no-param-reassign
-
-  return merge(
-    config,
-    getDefineConfig(settings),
-    getFilesConfig(settings),
-    getResolveConfig(settings),
-    {
-      resolve: {
-        modules: [
-          // Resolve dependencies to other @bundle-stats packages when linked
-          path.join(__dirname, '../../node_modules'),
-          path.join(__dirname, '../../ui/node_modules'),
-          path.join(__dirname, '../../utils/node_modules'),
-        ],
-      },
+  // Enable css-modules
+  // eslint-disable-next-line no-param-reassign
+  config.module.rules[3].use[1].options = {
+    modules: {
+      auto: true,
     },
-  );
+  };
+
+  return merge(config, getDefineConfig(settings), getResolveConfig(settings), {
+    resolve: {
+      modules: [
+        // Resolve dependencies to other @bundle-stats packages when linked
+        path.join(__dirname, '../../node_modules'),
+        path.join(__dirname, '../../ui/node_modules'),
+        path.join(__dirname, '../../utils/node_modules'),
+      ],
+    },
+  });
 };
