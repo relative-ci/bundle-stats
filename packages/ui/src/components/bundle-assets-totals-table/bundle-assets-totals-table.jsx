b2fa86eb1ec2ad1e4ea183a4f4dee2eefa4cfddc
Vio <vio@beanon.com>
2021-12-13 23:24:08 +0100

    feat(ui): BundleAssetsTotalsTable - add budgets

--- packages/ui/src/components/bundle-assets-totals-table/bundle-assets-totals-table.jsx
+++ packages/ui/src/components/bundle-assets-totals-table/bundle-assets-totals-table.jsx
@@ -1,11 +1,15 @@
 import React, { useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { get } from 'lodash';
 import { getBundleAssetsFileTypeComponentLink } from '@bundle-stats/utils';
 import * as webpack from '@bundle-stats/utils/lib-esm/webpack';
 
 import { ASSETS_SIZES_FILE_TYPE_MAP } from '../../constants';
+import { FlexStack } from '../../layout/flex-stack';
 import { MetricsTable } from '../metrics-table';
 import { ComponentLink } from '../component-link';
+import { BudgetInfo } from '../budget-info';
+import css from './bundle-assets-totals-table.module.css';
 
 export const BundleAssetsTotalsTable = ({
   className,
@@ -13,15 +17,23 @@ export const BundleAssetsTotalsTable = ({
   customComponentLink: CustomComponentLink,
 }) => {
   const items = useMemo(() => webpack.compareBySection.sizes(jobs), [jobs]);
+  const budgets = jobs[0].insights?.webpack?.budgets;
 
   const renderRowHeader = useCallback(
     (item) => {
       const fileType = ASSETS_SIZES_FILE_TYPE_MAP[item.key];
       const { section, title, params } = getBundleAssetsFileTypeComponentLink(fileType, item.label);
+      const [_, ...metricSlugs] = item.key.split('.');
+      const metricKey = metricSlugs.join('.');
+      const budget = get(budgets, metricKey);
+      const metric = webpack.getMetricType(metricKey);
 
       return (
         <CustomComponentLink section={section} title={title} params={params}>
-          {item.label}
+          <FlexStack space="xxsmall" className={css.itemTitleText}>
+            <span>{item.label}</span>
+            {budget && <BudgetInfo budget={budget} metric={metric} />}
+          </FlexStack>
         </CustomComponentLink>
       );
     },
