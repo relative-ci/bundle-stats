30b47b2da2eea4f584c2b719dc768c78cb7de46a
Vio <vio@beanon.com>
2022-01-22 16:47:12 +0100

    enhance(ui): Delta - use only inverted veration

--- packages/ui/src/components/delta/delta.stories.jsx
+++ packages/ui/src/components/delta/delta.stories.jsx
@@ -7,45 +7,12 @@ import { Delta } from '.';
 const stories = storiesOf('Components/Delta', module);
 stories.addDecorator(getWrapperDecorator());
 
-stories.add('default', () => (
-  <Delta
-    displayValue="20%"
-    deltaType="POSITIVE"
-  />
-));
+stories.add('default', () => <Delta displayValue="20%" deltaType="POSITIVE" />);
 
-stories.add('negative', () => (
-  <Delta
-    displayValue="20%"
-    deltaType="NEGATIVE"
-  />
-));
+stories.add('negative', () => <Delta displayValue="20%" deltaType="NEGATIVE" />);
 
-stories.add('slightlyNegative', () => (
-  <Delta
-    displayValue="1%"
-    deltaType="LOW_NEGATIVE"
-  />
-));
+stories.add('slightlyNegative', () => <Delta displayValue="1%" deltaType="LOW_NEGATIVE" />);
 
-stories.add('slightlyPositive', () => (
-  <Delta
-    displayValue="1%"
-    deltaType="LOW_POSITIVE"
-  />
-));
+stories.add('slightlyPositive', () => <Delta displayValue="1%" deltaType="LOW_POSITIVE" />);
 
-stories.add('empty', () => (
-  <Delta
-    displayValue="0%"
-    deltaType="NO_CHANGE"
-  />
-));
-
-stories.add('inverted', () => (
-  <Delta
-    displayValue="1%"
-    deltaType="LOW_NEGATIVE"
-    inverted
-  />
-));
+stories.add('empty', () => <Delta displayValue="0%" deltaType="NO_CHANGE" />);
