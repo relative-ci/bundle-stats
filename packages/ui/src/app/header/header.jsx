8a9ed99cc9d553cb4206c3c71d6fea4aef6d38af
Vio <vio@beanon.com>
2022-01-23 19:59:42 +0100

    enhance(ui): App - smaller header

--- packages/ui/src/app/header/header.jsx
+++ packages/ui/src/app/header/header.jsx
@@ -12,15 +12,19 @@ import css from './header.module.css';
 export const Header = ({ className, jobs }) => (
   <Container as="header" className={cx(css.root, className)}>
     <FlexStack space="small" className={css.inner}>
-      <a
-        className={css.branding}
-        href="https://github.com/relative-ci/bundle-stats"
-        target="_blank"
-        rel="noopener noreferrer nofollow"
-      >
-        <Logo className={css.brandingLogo}>BundleStats</Logo>
-      </a>
       <JobsHeader className={css.jobs} jobs={jobs} />
+      <div className={css.options}>
+        <a
+          href="https://github.com/relative-ci/bundle-stats"
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+          title="View bundle-stats on GitHub"
+        >
+          <Logo className={css.github} kind="github">
+            GitHub
+          </Logo>
+        </a>
+      </div>
     </FlexStack>
   </Container>
 );
