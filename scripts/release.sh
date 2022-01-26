301f722241e9af9a95a17f6f2f99c0108e4d7b7c
Vio <vio@beanon.com>
2022-01-26 22:12:06 +0100

    FIXUP

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,7 @@ const tag = execSync('git tag -l --points-at HEAD').toString().trim();
 const matchTag = tag.match(/^v\d*\.\d*\.\d*-(\w*)\..*$/);
 const distTag = (matchTag && matchTag[1]) || 'latest';
 
-const options = ['publish', 'from-git', `--dist-tag ${distTag}`, '--yes' ];
+const options = ['publish', 'from-git', `--dist-tag ${distTag}`, '--no-git-reset', '--yes' ];
 
 console.log(`Running lerna with "${options.join(' ')}"`);
 
