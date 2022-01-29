9e635753e57a4a16cc6e43d3658a499865ded1c4
Vio <vio@beanon.com>
2022-01-29 19:43:30 +0100

    Revert "fix: Remove setup-node cache input"
    This reverts commit 5a9db830ef164b9113318e844266593c6114c7b3.

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,16 +6,7 @@ const tag = execSync('git tag -l --points-at HEAD').toString().trim();
 const matchTag = tag.match(/^v\d*\.\d*\.\d*-(\w*)\..*$/);
 const distTag = (matchTag && matchTag[1]) || 'latest';
 
-const options = [
-  'publish',
-  'from-git',
-  `--dist-tag ${distTag}`,
-  '--no-git-reset',
-  '--no-changelog',
-  '--no-git-tag-version',
-  '--no-push',
-  '--yes'
-];
+const options = ['publish', 'from-git', `--dist-tag ${distTag}`, '--yes' ];
 
 console.log(`Running lerna with "${options.join(' ')}"`);
 
