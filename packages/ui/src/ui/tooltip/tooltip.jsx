64e1644b7414c5476e7c93780e229e1f463ae8be
Vio <vio@beanon.com>
2022-02-27 18:29:24 +0100

    refactor(ui): Use ariakit for tooltips

--- packages/ui/src/ui/tooltip/tooltip.jsx
+++ packages/ui/src/ui/tooltip/tooltip.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import {
   Tooltip as UITooltip,
+  TooltipAnchor as UITooltipAnchor,
   TooltipArrow as UITooltipArrow,
-  TooltipReference as UITooltipReference,
   useTooltipState,
-} from 'reakit/Tooltip';
+} from 'ariakit/tooltip';
 
 import css from './tooltip.module.css';
 
@@ -21,26 +21,28 @@ export const Tooltip = (props) => {
     ...restProps
   } = props;
 
-  const rootClassName = cx(css.root, className);
-  const tooltipProps = useTooltipState({
-    baseId: process.env.NODE_ENV === 'test' && 'id-test',
-    placement: 'top',
-  });
+  const tooltip = useTooltipState({ placement: 'top' });
+  const baseId = process.env.NODE_ENV === 'test' && 'id-test';
 
   return (
     <>
-      <UITooltipReference
+      <UITooltipAnchor
         as={Component}
-        className={rootClassName}
-        {...ref ? { ref } : {}}
+        className={cx(css.root, className)}
+        state={tooltip}
+        {...(ref ? { ref } : {})}
         {...restProps}
-        {...tooltipProps}
+        {...(baseId && { id: baseId })}
       >
         {children}
-      </UITooltipReference>
+      </UITooltipAnchor>
       {title && (
-        <UITooltip {...tooltipProps} className={cx(css.tooltip, darkMode && css.tooltipDarkMode)}>
-          <UITooltipArrow {...tooltipProps} className={css.arrow} size={12} />
+        <UITooltip
+          state={tooltip}
+          className={cx(css.tooltip, darkMode && css.tooltipDarkMode)}
+          {...(baseId && { id: `${baseId}-tooltip` })}
+        >
+          <UITooltipArrow state={tooltip} className={css.arrow} size={12} />
           {title}
         </UITooltip>
       )}
