cf71d1bca483588df95369b123ef0313a0c6f93f
Vio <vio@beanon.com>
2022-04-12 00:50:10 +0200

    fix(utils): Format - handle convert-units falsy case

--- packages/utils/src/utils/__tests__/format.js
+++ packages/utils/src/utils/__tests__/format.js
@@ -1,16 +1,24 @@
 import {
-  formatCommit, formatDuration, formatFileSize, formatNumber, formatPercentage,
+  formatCommit,
+  formatDuration,
+  formatFileSize,
+  formatNumber,
+  formatPercentage,
 } from '../format';
 
 describe('format', () => {
   test('formatFileSize', () => {
+    expect(formatFileSize()).toEqual('0B');
     expect(formatFileSize(0)).toEqual('0B');
+    expect(formatFileSize(0.1)).toEqual('0.1B');
     expect(formatFileSize(100)).toEqual('100B');
     expect(formatFileSize(100000)).toEqual('97.66KB');
   });
 
   test('formatDuration', () => {
-    expect(formatDuration(0)).toEqual('0ns');
+    expect(formatDuration()).toEqual('0ms');
+    expect(formatDuration(0)).toEqual('0ms');
+    expect(formatDuration(0.1)).toEqual('100mu');
     expect(formatDuration(100)).toEqual('100ms');
     expect(formatDuration(2200)).toEqual('2.2s');
   });
