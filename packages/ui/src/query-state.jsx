0b33e9c5836e0cfaef0d9f1eeb2306f20a5490d1
Vio <vio@beanon.com>
2022-08-22 19:19:39 +0200

    chore: Upgrade use-query-params setup

--- packages/ui/src/query-state.jsx
+++ packages/ui/src/query-state.jsx
@@ -1,16 +1,19 @@
 import React, { useCallback } from 'react';
-import { Route, useHistory, useLocation } from 'react-router-dom';
 import { JsonParam, QueryParamProvider, useQueryParams } from 'use-query-params';
+import { ReactRouter5Adapter } from 'use-query-params/adapters/react-router-5'
+import { parse, stringify } from 'query-string'
 import { isEqual, merge } from 'lodash';
 
-export const QueryStateProvider = (props) => {
-  const history = useHistory();
-  const location = useLocation();
-
-  return (
-    <QueryParamProvider ReactRouterRoute={Route} location={location} history={history} {...props} />
-  );
-};
+export const QueryStateProvider = (props) => (
+  <QueryParamProvider
+    adapter={ReactRouter5Adapter}
+    options={{
+      searchStringToObject: parse,
+      objectToSearchString: stringify,
+    }}
+    {...props}
+  />
+);
 
 export const useComponentQueryState = (componentName) => {
   const [search, setSearch] = useQueryParams({
