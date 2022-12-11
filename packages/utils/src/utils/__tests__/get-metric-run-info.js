4096801c2f05c9830fe661ba29469a45f70081a0
Vio <vio@beanon.com>
2022-12-11 16:06:18 +0100

    test(utils): Fix getMetricRunInfo cases

--- packages/utils/src/utils/__tests__/get-metric-run-info.js
+++ packages/utils/src/utils/__tests__/get-metric-run-info.js
@@ -1,18 +1,48 @@
 import { getMetricRunInfo, getGlobalMetricType } from '../metrics';
 
-describe('utils / metrics / getMetricRunInfo', () => {
-  test('should return the info', () => {
-    const metric = getGlobalMetricType('webpack.assets.totalSizeByTypeALL');
-    const info = getMetricRunInfo(metric, 11000, 10000);
-    const infoWithoutBaseline = getMetricRunInfo(metric, 10000);
-    const infoWithSlightlyNegativeDelta = getMetricRunInfo(metric, 10001, 10000);
-    const infoWithOverlyPositiveDelta = getMetricRunInfo(metric, 4000, 10000);
+const metric = getGlobalMetricType('webpack.totalSizeByTypeALL');
 
-    expect({
-      info,
-      infoWithoutBaseline,
-      infoWithSlightlyNegativeDelta,
-      infoWithOverlyPositiveDelta,
-    }).toMatchSnapshot();
+describe('utils / metrics / getMetricRunInfo', () => {
+  test('should return metric run info', () => {
+    expect(getMetricRunInfo(metric, 11264, 10240)).toEqual({
+      delta: 1024,
+      deltaPercentage: 10,
+      deltaType: 'NEGATIVE',
+      displayDelta: '+1KiB',
+      displayDeltaPercentage: '+10%',
+      displayValue: '11KiB',
+      value: 11264,
+    });
+  });
+
+  test('should return when the baseline is missing', () => {
+    expect(getMetricRunInfo(metric, 10240)).toEqual({
+      displayValue: '10KiB',
+      value: 10240,
+    });
+  });
+
+  test('should return low negative delta type', () => {
+    expect(getMetricRunInfo(metric, 10496, 10240)).toEqual({
+      delta: 256,
+      deltaPercentage: 2.5,
+      deltaType: 'LOW_NEGATIVE',
+      displayDelta: '+256B',
+      displayDeltaPercentage: '+2.5%',
+      displayValue: '10.25KiB',
+      value: 10496,
+    });
+  });
+
+  test.only('should return high positive delta  type', () => {
+    expect(getMetricRunInfo(metric, 4096, 10240)).toEqual({
+      delta: -6144,
+      deltaPercentage: -60,
+      deltaType: 'HIGH_POSITIVE',
+      displayDelta: '-6KiB',
+      displayDeltaPercentage: '-60%',
+      displayValue: '4KiB',
+      value: 4096,
+    });
   });
 });
