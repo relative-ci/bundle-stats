2f260e32dee8ee79acb9907b4627c499407d8663
Vio <vio@beanon.com>
2022-12-17 15:12:31 +0100

    fix(utils): Export constants

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -1,6 +1,7 @@
 export * from './browsertime';
 export * from './config';
 // export * as I18N from './i18n';
+export * from './constants';
 export * from './jobs';
 export * from './report';
 export * from './utils';
