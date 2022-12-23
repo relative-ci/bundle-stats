8c94ff51d252f5ca3e45359d24f756be7db8dd66
Vio <vio@beanon.com>
2022-12-23 18:11:28 +0100

    feat(ui): ModuleInfo - display reasons

--- packages/ui/src/components/module-info/module-info.jsx
+++ packages/ui/src/components/module-info/module-info.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import isEmpty from 'lodash/isEmpty';
 import noop from 'lodash/noop';
-import { getBundleModulesByChunk } from '@bundle-stats/utils';
+import { getBundleModulesByChunk, getBundleModulesBySearch } from '@bundle-stats/utils';
 
 import { Stack } from '../../layout/stack';
+import { FlexStack } from '../../layout/flex-stack';
 import { FileName } from '../../ui/file-name';
+import { Tag } from '../../ui/tag';
 import { ComponentLink } from '../component-link';
 import css from './module-info.module.css';
 
@@ -21,25 +24,34 @@ export const ModuleInfo = (props) => {
   } = props;
 
   return (
-    <Stack space="xsmall" className={className}>
-      {item.runs.map((run, index) => {
-        const TitleComponent = index !== 0 ? 'h4' : 'h3';
-        const key = `module-info-${run?.name || index}-${index}`;
+    <Stack space="small" className={cx(css.root, className)}>
+      {item.runs.map((run, index) => (
+        <Stack space="xsmall" key={`module-info-${run?.name || index}-${index}`} className={css.run}>
+          <Stack space="xxxsmall">
+            <FlexStack space="xxxsmall" alignItems="center" as="h4" className={css.title}>
+              <span>{labels[index]}</span>
+              <Tag className={css.titleTag}>{index === 0 ? 'current' : 'baseline'}</Tag>
+            </FlexStack>
+            <FileName className={css.fileName} as="code" name={run?.name || '-'} />
+          </Stack>
 
-        return (
-          <Stack space="xxxsmall" key={key}>
-            <TitleComponent>{labels[index]}</TitleComponent>
-            {index === 0 && !isEmpty(run?.chunkIds) && (
-              <div className={css.chunks}>
-                <span className={css.chunksTitle}>Chunks:</span>
-                {run.chunkIds.map((chunkId) => {
-                  const chunk = chunks?.find(({ id }) => id === chunkId);
+          {/**
+            * Render chunks only for the current run
+            * we do not have the correct chunk ids for baselines
+            */}
+          {index === 0 && !isEmpty(run?.chunkIds) && (
+            <Stack className={css.chunks}>
+              <h5>Chunks</h5>
+              <ul className={css.list}>
+              {run.chunkIds.map((chunkId) => {
+                const chunk = chunks?.find(({ id }) => id === chunkId);
 
-                  if (!chunk) {
-                    return null;
-                  }
+                if (!chunk) {
+                  return null;
+                }
 
-                  return (
+                return (
+                  <li>
                     <CustomComponentLink
                       {...getBundleModulesByChunk(chunkIds, chunkId)}
                       onClick={onClick}
@@ -47,15 +59,33 @@ export const ModuleInfo = (props) => {
                     >
                       {chunk.name}
                     </CustomComponentLink>
-                  );
-                })}
-              </div>
-            )}
+                  </li>
+                );
+              })}
+              </ul>
+            </Stack>
+          )}
 
-            <FileName className={css.fileName} as="code" name={run?.name || '-'} />
-          </Stack>
-        );
-      })}
+          {run?.reasons && (
+            <Stack>
+              <h5>Reasons</h5>
+              <ul className={css.list}>
+                {run.reasons.map((reason) => (
+                  <li key={reason}>
+                    <CustomComponentLink
+                      {...getBundleModulesBySearch(`^${reason}$`)}
+                      onClick={onClick}
+                      className={css.reasonLink}
+                      >
+                      <FileName name={reason} className={css.reason} />
+                    </CustomComponentLink>
+                  </li>
+                ))}
+              </ul>
+            </Stack>
+          )}
+        </Stack>
+      ))}
     </Stack>
   );
 };
