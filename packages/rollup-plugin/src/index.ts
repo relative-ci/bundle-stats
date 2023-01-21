9a763a298ac8b82edb341707ab66a8c4a80bfce4
Vio <vio@beanon.com>
2023-01-21 17:24:40 +0100

    feat: Add rollup-plugin

---
+++ packages/rollup-plugin/src/index.ts
@@ -0,0 +1,23 @@
+import { Plugin } from 'rollup';
+import { bundleToWebpackStats } from 'rollup-plugin-webpack-stats';
+import { ReportOptions, generateReports } from '@bundle-stats/cli-utils';
+
+const NAME = 'bundleStats';
+
+interface BundleStatsOptions extends ReportOptions {}
+
+export const bundleState = (options: BundleStatsOptions = {}): Plugin => ({
+  name: NAME,
+  async generateBundle(_, bundle) {
+    const source = bundleToWebpackStats(bundle);
+    const reports = await generateReports(source, options);
+
+    Object.entries(reports).forEach(([fileName, output]) => {
+      this.emitFile({
+        type: 'asset',
+        fileName,
+        source: output,
+      });
+    });
+  },
+});
