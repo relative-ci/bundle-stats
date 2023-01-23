d0eee44a85e5e806cc387f471e450fdef03d3910
Vio <vio@beanon.com>
2023-01-23 22:39:52 +0100

    refactor(cli): Convert cli to typescript

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import yargs from 'yargs';
+// @ts-ignore
 import updateNotifier from 'update-notifier';
 
 import run from './run';
@@ -7,19 +8,21 @@ import run from './run';
 const DEFAULT_OUTPUT_DIR = './dist';
 const WEEK_IN_MS = 1000 * 60 * 60 * 24 * 7;
 
-export default async function cli(packageInfo) {
+export default async function cli(pkg: object) {
   // Show update notification
-  const notifier = updateNotifier({ pkg: packageInfo, updateCheckInterval: WEEK_IN_MS });
+  const notifier = updateNotifier({ pkg, updateCheckInterval: WEEK_IN_MS });
 
   if (notifier.update) {
     notifier.notify();
   }
 
   // Generate demo report and skip processing
-  const { demo } = yargs.parse();
+  const { demo } = yargs.options({ demo: { type: 'boolean', default: false } }).parseSync();
 
   if (demo) {
     await run({
+      baseline: false,
+      compare: false,
       html: true,
       json: true,
       outDir: DEFAULT_OUTPUT_DIR,
@@ -37,55 +40,46 @@ export default async function cli(packageInfo) {
     .demandCommand(1, 'Webpack stats file path is required.')
     .option('out-dir', {
       description: 'Output directory',
+      type: 'string',
       default: DEFAULT_OUTPUT_DIR,
     })
-
     .option('compare', {
       description: 'Use local saved stats for comparison',
-      boolean: true,
+      type: 'boolean',
       default: true,
     })
     .option('baseline', {
       description: 'Save current stats as baseline',
-      boolean: true,
+      type: 'boolean',
       default: false,
     })
-
     .option('html', {
       description: 'Save HTML report',
-      boolean: true,
+      type: 'boolean',
       default: true,
     })
     .option('json', {
       description: 'Save JSON data',
-      boolean: true,
+      type: 'boolean',
       default: false,
     })
     .option('demo', {
       description: 'Generate demo reports',
+      type: 'boolean',
       default: false,
     })
     .alias('d', 'out-dir')
     .alias('h', 'help')
     .alias('v', 'version')
-    .help().argv;
-
-  const {
-    compare,
-    baseline,
-
-    html,
-    json,
-    outDir,
-    _: artifactFilepaths,
-  } = args;
+    .help()
+    .parseSync();
 
   await run({
-    baseline,
-    compare,
-    html,
-    json,
-    outDir,
-    artifactFilepaths,
+    baseline: args.baseline,
+    compare: args.compare,
+    html: args.html,
+    json: args.json,
+    outDir: args.outDir,
+    artifactFilepaths: args._ as Array<string>,
   });
 }
