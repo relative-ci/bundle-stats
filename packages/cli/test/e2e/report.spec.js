8f34e028033a605ca63c57335b1d65b53f26440f
Vio <vio@beanon.com>
2023-01-24 21:22:59 +0100

    test(cli): E2E - screenshots for each browser

--- packages/cli/test/e2e/report.spec.js
+++ packages/cli/test/e2e/report.spec.js
@@ -3,37 +3,37 @@ import { argosScreenshot } from '@argos-ci/playwright';
 
 const BASE_PATH = '/bundle-stats.html';
 
-test('should load overview panel', async ({ page }) => {
+test('should load overview panel', async ({ page, browserName }) => {
   await page.goto(BASE_PATH);
   await expect(page).toHaveTitle(/^Bundle Size — .*\./);
-  await argosScreenshot(page, 'overview', { fullPage: false });
+  await argosScreenshot(page, `overview-${browserName}`, { fullPage: false });
   await expect(page.getByRole('link', { name: 'Overview', exact: true })).toHaveAttribute(
     'aria-current',
     'page',
   );
 });
 
-test('should load assets panel', async ({ page }) => {
+test('should load assets panel', async ({ page, browserName }) => {
   await page.goto(`${BASE_PATH}#/assets`);
-  await argosScreenshot(page, 'assets', { fullPage: false });
+  await argosScreenshot(page, `assets-${browserName}`, { fullPage: false });
   await expect(page.getByRole('link', { name: 'Assets', exact: true })).toHaveAttribute(
     'aria-current',
     'page',
   );
 });
 
-test('should load modules panel', async ({ page }) => {
+test('should load modules panel', async ({ page, browserName }) => {
   await page.goto(`${BASE_PATH}#/modules`);
-  await argosScreenshot(page, 'modules', { fullPage: false });
+  await argosScreenshot(page, `modules-${browserName}`, { fullPage: false });
   await expect(page.getByRole('link', { name: 'Modules', exact: true })).toHaveAttribute(
     'aria-current',
     'page',
   );
 });
 
-test('should load packages panel', async ({ page }) => {
+test('should load packages panel', async ({ page, browserName }) => {
   await page.goto(`${BASE_PATH}#/packages`);
-  await argosScreenshot(page, 'packages', { fullPage: false });
+  await argosScreenshot(page, `packages-${browserName}`, { fullPage: false });
   await expect(page.getByRole('link', { name: 'Packages', exact: true })).toHaveAttribute(
     'aria-current',
     'page',
