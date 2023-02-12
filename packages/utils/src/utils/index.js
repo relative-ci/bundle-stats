e9431411e1f4a04d70fbca2b998098e0cf23ced3
Vio <vio@beanon.com>
2023-02-12 17:55:07 +0100

    refactor: Extract getInsightList

--- packages/utils/src/utils/index.js
+++ packages/utils/src/utils/index.js
@@ -1,6 +1,7 @@
 export * from './component-links';
 export * from './delta';
 export * from './date';
+export * from './insights';
 export * from './file-types';
 export * from './format';
 export * from './metrics';
---
+++ packages/utils/src/utils/insights.ts
@@ -0,0 +1,52 @@
+import { InsightType, JobInsight, JobInsights } from '../constants';
+import { BUNDLE_PACKAGES_DUPLICATE, BUNDLE_PACKAGES_COUNT } from './component-links';
+
+interface InsightEntry {
+  /**
+   * Inight name
+   */
+  name: string;
+  /**
+   * Insight data
+   */
+  insight: JobInsight;
+  /**
+   * Custom link props
+   */
+  link: any;
+}
+
+/**
+ * Get the insigh list ordered by level type
+ */
+export const getInsightList = (insights: Partial<JobInsights['webpack']>): Array<InsightEntry> => {
+  const { duplicatePackages, newPackages } = insights;
+
+  const insightsByLevel: Record<InsightType, Array<InsightEntry>> = {
+    [InsightType.ERROR]: [],
+    [InsightType.WARNING]: [],
+    [InsightType.INFO]: [],
+  };
+
+  if (duplicatePackages) {
+    insightsByLevel[duplicatePackages.type].push({
+      name: 'duplicatePackages',
+      insight: duplicatePackages,
+      link: BUNDLE_PACKAGES_DUPLICATE,
+    });
+  }
+
+  if (newPackages) {
+    insightsByLevel[newPackages.type].push({
+      name: 'newPackages',
+      insight: newPackages,
+      link: BUNDLE_PACKAGES_COUNT,
+    });
+  }
+
+  return [
+    ...(insightsByLevel[InsightType.ERROR] || []),
+    ...(insightsByLevel[InsightType.WARNING] || []),
+    ...(insightsByLevel[InsightType.INFO] || []),
+  ];
+};
