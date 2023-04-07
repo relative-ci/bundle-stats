6047dd58315ca141c964acd7892e46c836e6b965
Vio <vio@beanon.com>
2023-04-07 19:40:45 +0200

    feat(ui): PackageInfo - extract component and add details

--- packages/ui/src/components/bundle-packages/bundle-packages.jsx
+++ packages/ui/src/components/bundle-packages/bundle-packages.jsx
@@ -1,17 +1,10 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import {
-  PACKAGE_FILTERS,
-  PACKAGES_SEPARATOR,
-  PACKAGE_ID_SEPARATOR,
-  getBundleModulesBySearch,
-  getBundlePackagesByNameComponentLink,
-} from '@bundle-stats/utils';
+import { PACKAGE_FILTERS, PACKAGES_SEPARATOR } from '@bundle-stats/utils';
 
 import config from '../../config.json';
 import I18N from '../../i18n';
-import { Stack } from '../../layout/stack';
 import { FlexStack } from '../../layout/flex-stack';
 import { EmptySet } from '../../ui/empty-set';
 import { Filters } from '../../ui/filters';
@@ -19,12 +12,12 @@ import { HoverCard } from '../../ui/hover-card';
 import { SortDropdown } from '../../ui/sort-dropdown';
 import { Tag } from '../../ui/tag';
 import { Toolbar } from '../../ui/toolbar';
-import { FileName } from '../../ui/file-name';
 import { ComponentLink } from '../component-link';
 import { MetricsTable } from '../metrics-table';
 import { MetricsTableSearch } from '../metrics-table-search';
 import { MetricsTableOptions } from '../metrics-table-options';
 import { MetricsTableTitle } from '../metrics-table-title';
+import { PackageInfo } from '../package-info';
 import css from './bundle-packages.module.css';
 
 const getDropdownFilters = ({ compareMode, filters }) => ({
@@ -39,86 +32,19 @@ const getDropdownFilters = ({ compareMode, filters }) => ({
   },
 });
 
-
-const PackagePopoverContent = ({ name, fullName, path, duplicate, CustomComponentLink }) => {
-  const fallbackPackagePath = `node_modules/${fullName.split(PACKAGES_SEPARATOR).join('/node_modules/')}`;
-  const normalizedPackagePath = `${path || fallbackPackagePath}/`;
-  const [normalizedName, packageId] = name.split(PACKAGE_ID_SEPARATOR);
-
-  return (
-    <Stack space="xsmall" className={css.packageHoverCard}>
-      <Stack space="xxxsmall">
-        <h3 className={css.packageHoverCardTitle}>
-          {normalizedName}
-          {packageId && (
-            <span className={css.packageHoverCardTitleIndex}>
-              {`${PACKAGE_ID_SEPARATOR}${packageId}`}
-            </span>
-          )}
-        </h3>
-        <p className={css.packageHoverCardPath}>
-          <FileName className={css.packageHoverCardPathValue} name={normalizedPackagePath} />
-        </p>
-      </Stack>
-
-      <ul className={css.packageHoverCardList}>
-        <li className={css.packageHoverCardItem}>
-          <a
-            href={`https://www.npmjs.com/package/${normalizedName}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            npmjs.com
-          </a>
-        </li>
-        <li className={css.packageHoverCardItem}>
-          <a
-            href={`https://bundlephobia.com/result?p=${normalizedName}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            bundlephobia.com
-          </a>
-        </li>
-      </ul>
-
-      <Stack space="xxxsmall" className={css.packageHoverCardActions}>
-        {duplicate && (
-          <div>
-            <CustomComponentLink {...getBundlePackagesByNameComponentLink(normalizedName)}>
-              View all duplicate instances
-            </CustomComponentLink>
-          </div>
-        )}
-
-        <CustomComponentLink {...getBundleModulesBySearch(normalizedPackagePath)}>
-          Search modules by package path
-        </CustomComponentLink>
-      </Stack>
-    </Stack>
-  );
-};
-
-PackagePopoverContent.propTypes = {
-  name: PropTypes.string.isRequired,
-  path: PropTypes.string,
-  fullName: PropTypes.string.isRequired,
-  duplicate: PropTypes.bool.isRequired,
-  CustomComponentLink: PropTypes.elementType.isRequired,
-};
-
-PackagePopoverContent.defaultProps = {
-  path: '',
-};
-
-const PackageName = ({ packageName, path, showDuplicateFlag, row, CustomComponentLink }) => {
+const PackageName = ({ packageName, showDetails, row, labels, CustomComponentLink }) => {
   const [showHoverCard, setHoverCard] = useState(false);
-  const handleOnMouseEnter = useCallback(() => setHoverCard(true), [showHoverCard]);
+
+  const handleOnMouseEnter = useCallback(() => {
+    if (showDetails) {
+      setHoverCard(true);
+    }
+  }, [setHoverCard, showDetails]);
 
   const label = useMemo(
     () => (
       <>
-        {showDuplicateFlag && row.duplicate && (
+        {showDetails && row.duplicate && (
           <Tag
             className={css.packageNameTagDuplicate}
             title="Duplicate package"
@@ -129,7 +55,7 @@ const PackageName = ({ packageName, path, showDuplicateFlag, row, CustomComponen
         <span className={css.packageNameLabel}>{packageName}</span>
       </>
     ),
-    [packageName, showDuplicateFlag, row],
+    [packageName, showDetails, row],
   );
 
   if (!showHoverCard) {
@@ -141,12 +67,11 @@ const PackageName = ({ packageName, path, showDuplicateFlag, row, CustomComponen
   }
 
   return (
-    <HoverCard className={css.packageName} label={<span className={css.packageName}>{label}</span>}>
-      <PackagePopoverContent
+    <HoverCard label={label} className={css.packageName} hoverCardClassName={css.hoverCard}>
+      <PackageInfo
         name={packageName}
-        path={path}
-        fullName={row.label}
-        duplicate={row.duplicate}
+        item={row}
+        labels={labels}
         CustomComponentLink={CustomComponentLink}
       />
     </HoverCard>
@@ -155,8 +80,7 @@ const PackageName = ({ packageName, path, showDuplicateFlag, row, CustomComponen
 
 PackageName.propTypes = {
   packageName: PropTypes.string.isRequired,
-  path: PropTypes.string.isRequired,
-  showDuplicateFlag: PropTypes.bool.isRequired,
+  showDetails: PropTypes.bool.isRequired,
   row: PropTypes.shape({
     label: PropTypes.string,
     duplicate: PropTypes.bool,
@@ -169,19 +93,17 @@ PackageName.propTypes = {
   CustomComponentLink: PropTypes.elementType.isRequired,
 };
 
-const RowHeader = ({ row, CustomComponentLink }) => {
+const RowHeader = ({ row, labels, CustomComponentLink }) => {
   const packageNames = row.label.split(PACKAGES_SEPARATOR);
-  const { path } = row.runs[0] || {};
 
   return (
     <span className={css.packageNames}>
       {packageNames.map((packageName, index) => (
         <PackageName
           packageName={packageName}
-          path={path}
-          // Render duplicate flag only for the last entry
-          showDuplicateFlag={index === packageNames.length - 1}
           row={row}
+          labels={labels}
+          showDetails={index === packageNames.length - 1}
           CustomComponentLink={CustomComponentLink}
         />
       ))}
@@ -226,28 +148,40 @@ export const BundlePackages = (props) => {
     [jobs, filters],
   );
 
-  const metricsTableTitle = useMemo(() => (
-    <MetricsTableTitle
-      title={I18N.PACKAGES}
-      info={`${items.length}/${totalRowCount}`}
-      popoverInfo={I18N.PACKAGES_INFO}
-      popoverHref={config.documentation.packages}
-    />
-  ), [items, totalRowCount]);
-
-  const renderRowHeader = useCallback(
-    (row) => <RowHeader row={row} CustomComponentLink={CustomComponentLink} />,
-    [CustomComponentLink],
+  const metricsTableTitle = useMemo(
+    () => (
+      <MetricsTableTitle
+        title={I18N.PACKAGES}
+        info={`${items.length}/${totalRowCount}`}
+        popoverInfo={I18N.PACKAGES_INFO}
+        popoverHref={config.documentation.packages}
+      />
+    ),
+    [items, totalRowCount],
   );
 
-  const emptyMessage = useMemo(() => (
-    <EmptySet
-      resources="packages"
-      filtered={totalRowCount !== 0}
-      handleResetFilters={resetFilters}
-      handleViewAll={resetAllFilters}
-    />
-  ), [totalRowCount, resetFilters, resetAllFilters]);
+  const renderRowHeader = useCallback(
+    (row) => (
+      <RowHeader
+        row={row}
+        labels={jobs.map((job) => job?.label)}
+        CustomComponentLink={CustomComponentLink}
+      />
+    ),
+    [CustomComponentLink, jobs],
+  );
+
+  const emptyMessage = useMemo(
+    () => (
+      <EmptySet
+        resources="packages"
+        filtered={totalRowCount !== 0}
+        handleResetFilters={resetFilters}
+        handleViewAll={resetAllFilters}
+      />
+    ),
+    [totalRowCount, resetFilters, resetAllFilters],
+  );
 
   return (
     <section className={cx(css.root, className)}>
---
+++ packages/ui/src/components/package-info/package-info.tsx
@@ -0,0 +1,90 @@
+import React, { useMemo } from 'react';
+import {
+  PACKAGES_SEPARATOR,
+  PACKAGE_ID_SEPARATOR,
+  MetricRunInfo,
+  getBundleModulesBySearch,
+  getBundlePackagesByNameComponentLink,
+} from '@bundle-stats/utils';
+import { Package, MetaChunk } from '@bundle-stats/utils/types/webpack';
+
+import { Stack } from '../../layout/stack';
+import { ComponentLink } from '../component-link';
+import { FileName } from '../../ui/file-name';
+import { EntryInfo } from '../entry-info';
+import css from './package-info.module.css';
+
+interface PackageInfoProps {
+  name: string;
+  item: {
+    label: string;
+    duplicate?: boolean;
+    runs: Array<Package & MetricRunInfo>;
+  };
+  labels: Array<string>;
+  customComponentLink?: React.ElementType;
+}
+
+export const PackageInfo = (props: PackageInfoProps & React.ComponentProps<'div'>) => {
+  const {
+    className = '',
+    name,
+    item,
+    labels,
+    customComponentLink: CustomComponentLink = ComponentLink,
+  } = props;
+
+  const fallbackPackagePath = `node_modules/${item.label.split(PACKAGES_SEPARATOR).join('/node_modules/')}`;
+  const normalizedPackagePath = `${item.runs?.[0]?.path || fallbackPackagePath}/`;
+  const [normalizedName] = name.split(PACKAGE_ID_SEPARATOR);
+
+  const packageItem = useMemo(
+    () => ({
+      ...item,
+      label: name,
+    }),
+    [item, name],
+  );
+
+  return (
+    <EntryInfo item={packageItem} labels={labels} className={className}>
+      <p>Path: <FileName name={normalizedPackagePath} className={css.fileName} /></p>
+      <Stack space="xxxsmall" className={css.packageHoverCardActions}>
+        {item.duplicate && (
+          <div>
+            <CustomComponentLink {...getBundlePackagesByNameComponentLink(normalizedName)}>
+              View all duplicate instances
+            </CustomComponentLink>
+          </div>
+        )}
+
+        <CustomComponentLink {...getBundleModulesBySearch(normalizedPackagePath)}>
+          Search modules by package path
+        </CustomComponentLink>
+      </Stack>
+      <ul className={css.external}>
+        <li className={css.externalItem}>
+          <a
+            href={`https://www.npmjs.com/package/${normalizedName}`}
+            target="_blank"
+            rel="noreferrer"
+            className={css.externalLink}
+          >
+            npmjs.com
+          </a>
+        </li>
+        <li className={css.externalItem}>
+          <a
+            href={`https://bundlephobia.com/result?p=${normalizedName}`}
+            target="_blank"
+            rel="noreferrer"
+            className={css.externalLink}
+          >
+            bundlephobia.com
+          </a>
+        </li>
+      </ul>
+
+    </EntryInfo>
+  );
+};
