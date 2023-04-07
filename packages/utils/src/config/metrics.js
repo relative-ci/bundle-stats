6022db425030b495a02d2f09523571256eb13b54
Vio <vio@beanon.com>
2023-04-07 16:58:59 +0200

    style(utils): METRIC_TYPES - add types

--- packages/utils/src/config/metrics.js
+++ packages/utils/src/config/metrics.js
@@ -1,9 +1,4 @@
-import {
-  formatNumber,
-  formatDuration,
-  formatFileSize,
-  formatPercentage,
-} from '../utils/format';
+import { formatNumber, formatDuration, formatFileSize, formatPercentage } from '../utils/format';
 
 export const METRIC_TYPE_FILE_SIZE = 'METRIC_TYPE_FILE_SIZE';
 export const METRIC_TYPE_DURATION = 'METRIC_TYPE_DURATION';
@@ -11,6 +6,7 @@ export const METRIC_TYPE_NUMERIC = 'METRIC_TYPE_NUMERIC';
 export const METRIC_TYPE_SCORE = 'METRIC_TYPE_SCORE';
 export const METRIC_TYPE_PERCENTAGE = 'METRIC_TYPE_PERCENTAGE';
 
+/** @type {Record<string, import('../constants').MetricType>} */
 export const METRIC_TYPES = {
   [METRIC_TYPE_NUMERIC]: {
     biggerIsBetter: false,
