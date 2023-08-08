a35fcde39237c9ccbd5ebc62cd2319433f70794d
Vio <vio@beanon.com>
2023-08-08 18:44:46 +0200

    test(webpack-plugin): Add test case for compare with default baseline.json

--- packages/webpack-plugin/src/__tests__/webpack-plugin.test.js
+++ packages/webpack-plugin/src/__tests__/webpack-plugin.test.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import fs from 'fs/promises';
 import webpack from 'webpack';
 import MemoryFS from 'memory-fs';
 import { advanceTo } from 'jest-date-mock';
@@ -53,6 +54,63 @@ describe('webpack plugin', () => {
       );
       expect(baselineAsset).toBeTruthy();
       done();
+    });
+  });
+
+  describe('compare', () => {
+    // Create default baseline file
+    const outputDir = path.join(__dirname, '../../node_modules/.cache/bundle-stats');
+    const baselineFilepath = path.join(outputDir, 'baseline.json');
+
+    beforeEach(async () => {
+      await fs.mkdir(outputDir, { recursive: true });
+      await fs.writeFile(
+        baselineFilepath,
+        JSON.stringify({
+          assets: [
+            {
+              name: 'main.js',
+              size: 20,
+            },
+          ],
+        }),
+      );
+
+      jest.spyOn(global.console, 'info');
+      jest.spyOn(global.console, 'warn');
+    });
+
+    afterEach(async () => {
+      await fs.rm(baselineFilepath);
+    });
+
+    test('should compare with baseline.json', (done) => {
+      expect.assertions(6);
+
+      const compiler = webpack({
+        mode: 'production',
+        context: CONTEXT,
+        plugins: [new BundleStatsWebpackPlugin({ compare: true })],
+      });
+      compiler.outputFileSystem = new MemoryFS();
+
+      compiler.run((error, stats) => {
+        expect(error).toEqual(null);
+        expect(stats.hasErrors()).toBe(false);
+
+        /* eslint-disable no-console */
+        expect(console.warn).not.toBeCalledWith('Missing baseline stats, see "baseline" option.');
+        expect(console.info).toBeCalledWith(
+          'Read baseline from ../node_modules/.cache/bundle-stats/baseline.json',
+        );
+        expect(console.info).toBeCalledWith('Bundle Size — 27B (+35%).');
+        /* eslint-enable no-console */
+
+        const { assets } = stats.toJson({ source: false, assets: true });
+        const bundleStatsAsset = assets.find((asset) => asset.name.match(/bundle-stats\.html$/));
+        expect(bundleStatsAsset).toBeTruthy();
+        done();
+      });
     });
   });
 });
