bc7237398991ea5d4fe6effd9b41a9fca1b1b74b
Vio <vio@beanon.com>
2023-08-22 19:15:24 +0200

    refactor: Fixtures - extract webpack stats json

---
+++ fixtures/job.baseline.js
@@ -0,0 +1,14 @@
+const webpackStats = require('./webpack-stats.baseline.json');
+
+module.exports = {
+  branch: 'master',
+  buildNumber: '123',
+  buildUrl: '#ci-url',
+  commit: '428abb744923',
+  commitMessage: 'feat: Add Summary component',
+  createdAt: '2019-02-28T08:32:26.000Z',
+  internalBuildNumber: 123,
+  rawData: {
+    webpack: webpackStats,
+  },
+};
---
+++ fixtures/job.current.js
@@ -0,0 +1,14 @@
+const webpackStats = require('./webpack-stats.current.json');
+
+module.exports = {
+  branch: 'master',
+  buildNumber: '124',
+  buildUrl: '#ci-url',
+  commit: 'c13a3d3743b0',
+  commitMessage: 'refactor: Use common ui components',
+  createdAt: '2019-02-28T08:32:26.000Z',
+  internalBuildNumber: 124,
+  rawData: {
+    webpack: webpackStats,
+  },
+};
