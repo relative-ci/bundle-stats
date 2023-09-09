cd7fe02298da7f2554e6692a3646ea814fa76b38
Vio <vio@beanon.com>
2023-09-09 11:26:47 +0200

    buld: Test registry - add npmrc on registry setup

--- scripts/local-registry.sh
+++ scripts/local-registry.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Setup npmrc"
+npm config set registry=http://localhost:4873
+npm config set //localhost:4873/:_authToken=fooBar # Add a non empty token for npm 6
+
 echo "Run local registry"
 npx verdaccio --config ./verdaccio.yaml
 
