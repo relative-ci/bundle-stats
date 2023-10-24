752d925baf68900aa834531b2ae85f581d2822a6
Vio <vio@beanon.com>
2023-10-24 18:32:51 +0200

    test(utils): Component links - increase the filter size

--- packages/utils/src/utils/__tests__/component-links.ts
+++ packages/utils/src/utils/__tests__/component-links.ts
@@ -45,11 +45,20 @@ describe('component links', () => {
             search: 'query',
             filters: {
               changed: false,
+              'c.100': false,
+              'c.200': false,
+              'c.300': false,
+              'c.400': false,
+              'c.500': false,
+              'c.600': false,
+              'c.700': false,
+              'c.800': false,
+              'c.900': false,
             },
           },
         }),
       ).toEqual(
-        'ba=%7B%22search%22%3A%22%22%2C%22filters%22%3A%7B%22changed%22%3Atrue%7D%7D&bm=%7B%22search%22%3A%22query%22%2C%22filters%22%3A%7B%22changed%22%3Afalse%7D%7D',
+        'ba=%7B%22search%22%3A%22%22%2C%22filters%22%3A%7B%22changed%22%3Atrue%7D%7D&bm=%7B%22search%22%3A%22query%22%2C%22filters%22%3A%7B%22changed%22%3Afalse%2C%22c.100%22%3Afalse%2C%22c.200%22%3Afalse%2C%22c.300%22%3Afalse%2C%22c.400%22%3Afalse%2C%22c.500%22%3Afalse%2C%22c.600%22%3Afalse%2C%22c.700%22%3Afalse%2C%22c.800%22%3Afalse%2C%22c.900%22%3Afalse%7D%7D',
       );
     });
   });
