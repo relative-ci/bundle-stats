8d3c4b31d520d001f13848f38da098ef8daa047b
Vio <vio@beanon.com>
2023-10-24 19:31:34 +0200

    fix(ui): Component links - use custom encoder/decoder

--- packages/utils/src/utils/__tests__/component-links.ts
+++ packages/utils/src/utils/__tests__/component-links.ts
@@ -1,19 +1,38 @@
-import { decodeComponentStateQueryString, getComponentStateQueryString } from '../component-links';
+import { getComponentStateData, getComponentStateQueryString } from '../component-links';
 
 describe('component links', () => {
-  describe('decodeComponentStateQueryString', () => {
+  describe('getComponentState', () => {
     test('should return empty component state when query string does not contain any state', () => {
-      expect(decodeComponentStateQueryString('')).toEqual({});
-      expect(decodeComponentStateQueryString('a=1')).toEqual({ a: '1' });
+      expect(getComponentStateData('')).toEqual({});
+      expect(getComponentStateData('a=1')).toEqual({ a: '1' });
     });
-    test('should decode component state from query string', () => {
+    test('should decode component state from json ecoded query string (obsolete)', () => {
       expect(
-        decodeComponentStateQueryString(
+        getComponentStateData(
           'ba=%7B%22search%22%3A%22%22%2C%22filters%22%3A%7B%22changed%22%3Atrue%7D%7D&bm=%7B%22search%22%3A%22query%22%2C%22filters%22%3A%7B%22changed%22%3Afalse%7D%7D',
         ),
       ).toEqual({
         ba: {
-          search: '',
+          filters: {
+            changed: true,
+          },
+        },
+        bm: {
+          search: 'query',
+          filters: {
+            changed: false,
+          },
+        },
+      });
+    });
+
+    test('should decode component state from mixed encoder query string', () => {
+      expect(
+        getComponentStateData(
+          'ba=%7B%22search%22%3A%22%22%2C%22filters%22%3A%22changed-1%22%7D&bm=%7B%22search%22%3A%22query%22%2C%22filters%22%3A%22changed-0%22%7D',
+        ),
+      ).toEqual({
+        ba: {
           filters: {
             changed: true,
           },
@@ -27,6 +46,7 @@ describe('component links', () => {
       });
     });
   });
+
   describe('getComponentStateQueryString', () => {
     test('should return empty string when params are missing', () => {
       expect(getComponentStateQueryString()).toEqual('');
@@ -58,7 +78,7 @@ describe('component links', () => {
           },
         }),
       ).toEqual(
-        'ba=%7B%22search%22%3A%22%22%2C%22filters%22%3A%7B%22changed%22%3Atrue%7D%7D&bm=%7B%22search%22%3A%22query%22%2C%22filters%22%3A%7B%22changed%22%3Afalse%2C%22c.100%22%3Afalse%2C%22c.200%22%3Afalse%2C%22c.300%22%3Afalse%2C%22c.400%22%3Afalse%2C%22c.500%22%3Afalse%2C%22c.600%22%3Afalse%2C%22c.700%22%3Afalse%2C%22c.800%22%3Afalse%2C%22c.900%22%3Afalse%7D%7D',
+        'ba=%7B%22filters%22%3A%22changed-1%22%7D&bm=%7B%22search%22%3A%22query%22%2C%22filters%22%3A%22changed-0_c.100-0_c.200-0_c.300-0_c.400-0_c.500-0_c.600-0_c.700-0_c.800-0_c.900-0%22%7D',
       );
     });
   });
