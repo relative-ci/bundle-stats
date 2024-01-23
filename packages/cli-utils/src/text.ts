8b22b89ed6aa076732c38eef79ae62a372eb5d39
Vio <vio@beanon.com>
2024-01-23 19:26:46 +0100

    fix(cli-utils): Add log entry for baseline writing

--- packages/cli-utils/src/text.ts
+++ packages/cli-utils/src/text.ts
@@ -1,5 +1,6 @@
 export const BASELINE_MISSING = 'Baseline stats file is missing, see "baseline" option.';
 export const BASELINE_READING = 'Reading baseline data from';
+export const BASELINE_WRITING = 'Writing baseline data to';
 
 export const CLI_NO_COMPARE_MODE = 'Not in compare mode (see --compare).';
 export const CLI_MULTIPLE_STATS = 'Multiple stat files already set.';
