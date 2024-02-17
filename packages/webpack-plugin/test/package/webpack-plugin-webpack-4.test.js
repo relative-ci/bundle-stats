de1e5bfd31d664ee0bfc6eff165e1ecb6ce71d06
Vio <vio@beanon.com>
2024-02-17 16:17:28 +0100

    test(webpack-plugin): Replace snapshot testing with file output test

--- packages/webpack-plugin/test/package/webpack-plugin-webpack-4.test.js
+++ packages/webpack-plugin/test/package/webpack-plugin-webpack-4.test.js
@@ -1,26 +1,26 @@
+const path = require('path');
 const webpack4 = require('webpack4'); // eslint-disable-line
 const MemoryFS = require('memory-fs');
-const { advanceTo } = require('jest-date-mock');
 
 jest.mock('webpack', () => require('./node_modules/webpack4')); // eslint-disable-line
+jest.setTimeout(10 * 1000);
 
 const config = require('./app/webpack.config');
 
-advanceTo(new Date(2020, 10, 30));
-
-jest.setTimeout(10 * 1000);
-
 describe('webpack plugin package', () => {
   test('webpack4', (done) => {
+    const fs = new MemoryFS();
+
     expect.assertions(3);
 
     const compiler = webpack4(config);
-    compiler.outputFileSystem = new MemoryFS();
+    compiler.outputFileSystem = fs;
 
     compiler.run((error, stats) => {
       expect(error).toEqual(null);
       expect(stats.hasErrors()).toBe(false);
-      expect(stats.toJson({ source: false, assets: true }).assets).toMatchSnapshot();
+      const report = fs.readFileSync(path.join(__dirname, './dist/bundle-stats.html'), 'utf-8');
+      expect(report).toBeTruthy();
       done();
     });
   });
