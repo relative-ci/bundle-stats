90e20631ca7bb63dac36391f88ac2113740c70f8
Viorel Cojocaru <vio@beanon.com>
2025-01-14 22:47:22 +0100

    build(rollup-plugin): Bundle externals

--- packages/rollup-plugin/rollup.config.mjs
+++ packages/rollup-plugin/rollup.config.mjs
@@ -1,27 +1,20 @@
+import path from 'path';
+import url from 'url';
 import { defineConfig } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 
-export default defineConfig([
-  {
-    input: './src/index.ts',
-    output: {
-      dir: './dist',
-      format: 'esm',
-      entryFileNames: '[name].mjs',
-      chunkFileNames: 'chunks/[name].mjs',
-      sourcemap: true,
-    },
-    plugins: [typescript({ tsconfig: './tsconfig.json' })],
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  input: `${__dirname}/src/index.ts`,
+  output: {
+    dir: `${__dirname}/dist`,
+    format: 'commonjs',
+    sourcemap: true,
   },
-  {
-    input: './src/index.ts',
-    output: {
-      dir: './dist',
-      format: 'commonjs',
-      entryFileNames: '[name].cjs',
-      chunkFileNames: 'chunks/[name].cjs',
-      sourcemap: true,
-    },
-    plugins: [typescript({ tsconfig: './tsconfig.json' })],
-  },
-]);
+  plugins: [typescript({ tsconfig: './tsconfig.json' }), json(), nodeResolve(), commonjs()],
+});
