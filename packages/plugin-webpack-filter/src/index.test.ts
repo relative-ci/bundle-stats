23183a81de4ede52153ccd2a6af5a3ec01d50e07
Viorel Cojocaru <vio@beanon.com>
2025-03-07 23:37:54 +0100

    build(plugin-webpack-filter): Build with rollup

--- packages/plugin-webpack-filter/src/index.test.ts
+++ packages/plugin-webpack-filter/src/index.test.ts
@@ -1,9 +1,13 @@
 import filter from './index';
 
 /* eslint-disable import/no-relative-packages */
+// @ts-ignore share fixture from utils
 import webpackStats from '../../utils/__fixtures__/webpack-stats-1';
+// @ts-ignore share fixture from utils
 import webpackStatsExtracted from '../../utils/__fixtures__/webpack-stats-1.extracted';
+// @ts-ignore share fixture from utils
 import webpackStatsConcatenatedModules from '../../utils/__fixtures__/webpack-stats-3';
+// @ts-ignore share fixture from utils
 import webpackStatsConcatenatedModulesExtracted from '../../utils/__fixtures__/webpack-stats-3.extracted';
 /* eslint-enable import/no-relative-packages */
 
