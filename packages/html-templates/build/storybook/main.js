4c98b19bfc44a9b6bae5cdd8950a3784451b9111
Viorel Cojocaru <vio@beanon.com>
2026-08-10 23:04:16 +0200

    chore: Upgrade storybook to v10
    add addon-styling-webpack to support css modules without named exports

--- packages/html-templates/build/storybook/main.js
+++ packages/html-templates/build/storybook/main.js
@@ -7,7 +7,38 @@ function getAbsolutePath(value) {
 module.exports = {
   framework: getAbsolutePath('@storybook/react-webpack5'),
   stories: ['../../src/**/*.stories.@(jsx|tsx|mdx)'],
-  addons: [getAbsolutePath('@storybook/addon-webpack5-compiler-babel')],
+  addons: [
+    getAbsolutePath('@storybook/addon-webpack5-compiler-babel'),
+    {
+      name: getAbsolutePath('@storybook/addon-styling-webpack'),
+      options: {
+        rules: [
+          {
+            test: /\.css$/,
+            sideEffects: true,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                  // Keep the default import style used across the codebase
+                  // (`import css from './x.module.css'`) working, and preserve the
+                  // original class names (`as-is`) since they are accessed via
+                  // bracket notation, e.g. `css['tile-NO_CHANGE']`.
+                  modules: {
+                    auto: true,
+                    namedExport: false,
+                    exportLocalsConvention: 'asIs',
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
 
   docs: {
     autodocs: true,
