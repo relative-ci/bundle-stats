ac154c4f6c2897d31101ae3312bc7a45d9d5289c
Viorel Cojocaru <vio@beanon.com>
2026-08-24 00:27:29 +0200

    feat(utils): Add generateJobLabel

--- packages/utils/src/jobs/create-jobs.ts
+++ packages/utils/src/jobs/create-jobs.ts
@@ -1,19 +1,21 @@
 import { createJob } from './create-job';
 import { Job, SourceData } from '../constants';
+import { generateJobLabel } from './generate-job-label';
 
 /*
  * Create jobs from sources ([current, ..., baseline])
  */
 export const createJobs = (sources: Array<SourceData>): Array<Job> => {
   const jobs = [] as Array<Job>;
+  const sourcesLength = sources.length;
 
-  for (let i = sources.length - 1; i >= 0; i -= 1) {
+  for (let i = sourcesLength - 1; i >= 0; i -= 1) {
     const source = sources[i];
     const baseline = jobs.length > 0 ? jobs[jobs.length - 1] : undefined;
     const jobData = createJob(source, baseline);
 
-    const internalBuildNumber = sources.length - i;
-    const label = `Job #${internalBuildNumber}`;
+    const internalBuildNumber = sourcesLength - i;
+    const label = generateJobLabel(internalBuildNumber, sourcesLength, i);
 
     const job = {
       ...jobData,
