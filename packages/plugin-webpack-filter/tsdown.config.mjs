ccedee0e6f4b76cd114b8dcb7d0396a26e129c85
Viorel Cojocaru <vio@beanon.com>
2026-08-23 13:44:30 +0200

    build(plugin-webpack-filter): Build with tsdown

---
+++ packages/plugin-webpack-filter/tsdown.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: './src/index.ts',
+  outDir: 'lib',
+  format: ['cjs', 'esm'],
+  dts: true,
+  sourcemap: true,
+  hash: false,
+  minify: false,
+  deps: {
+    neverBundle: ['webpack'],
+  },
+});
